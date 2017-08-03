2cb8fc2f6aed7ab85c820ca46e2f045e93f94af3
manishavi <manishalal972@gmail.com>
2017-08-03 16:52:05 -0500

    changes

---
+++ Routing-Movies/Movies/src/components/MoviesList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MoviesList = () => <h1>MoviesList</h1>;
+
+export default MoviesList;
---
+++ Routing-Movies/Movies/src/components/MoviesTitle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MoviesTitle = () => <h1>MoviesTitle</h1>;
+
+export default MoviesTitle;
