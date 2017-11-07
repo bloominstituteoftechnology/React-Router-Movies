02ba6c7b97e9f137359389ee0a92fce7f7e9ea10
Sam Kim <sams404link@gmail.com>
2017-11-06 17:41:39 -0800

    Routing Sprint updated

---
+++ movies/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Navigation from './components/Navigation';
+import { Route } from 'react-router-dom';
+import { Home, MovieList } from './components';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+     <Navigation/>
+     <Route exact = {true} path = "/" component = {Home}/>
+     <Route path = "/movielist" component = {MovieList}/>
+      </div>
+    );
+  }
+}
+
+export default App;
