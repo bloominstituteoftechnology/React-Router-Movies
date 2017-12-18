736b2de12a420a5bf44b1693bcd4fa13e4fed5a2
sarika <sarikaresume@gmail.com>
2017-12-18 14:29:56 -0800

    hello

---
+++ homemovie/src/components/home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+BrowserRouter as Router,
+Link, 
+Route } from 'react-router-dom';
+
+export default class Home extends Component {
+    constructor()
+    super();
+
+}
+
+render () {
+    return(
+        <div>
+
+            <h1> HOME </h1>
+
+            </div>
+    );
+};
+}
