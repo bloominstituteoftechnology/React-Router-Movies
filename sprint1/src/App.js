3f227280d32100521c3dd9e9f6c3b1c4075afbd7
atiffany <abby.tiffany@gmail.com>
2017-12-18 15:14:32 -0500

    react components and route set up

---
+++ sprint1/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Home from './components/Home';
+import Movies from './components/Movies';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Route exact path = '/' component = {Home} />
+        <Route exact path = '/movies' component = {Movies} />
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
