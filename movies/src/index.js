de0cbce22721845297dc0af55cff2005ac540e3a
Jason Metten <metten@gmail.com>
2017-12-18 14:22:57 -0600

    Set up dependencies

---
+++ movies/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
