68bb1e4f314d34fcce7f2a349154d0aa04fc4104
Ting Wang <felix.wang.1026@gmail.com>
2017-12-19 12:35:18 -0500

    Modified the reducers

--- movies/src/reducers/index.js
+++ movies/src/reducers/index.js
@@ -1,10 +1,30 @@
-import MoviesReducer from './MoviesReducer';
-import SelectMovieReducer from './SelectMovie';
+import { GET_MOVIES, ADD_NEW_MOVIE, GET_SELECT_MOVIE } from '../actions';
 import { combineReducers } from 'redux';
+
+const MoviesReducer =  (movies = [], action) => {
+  switch (action.type) {
+    case GET_MOVIES:
+      return action.payload.data;
+    case ADD_NEW_MOVIE:
+    const movie = action.payload.data;
+      return movies.concat(movie);
+    default:
+      return movies;
+  }
+};
+
+const GetSelectMovieReducer = (selectMovie = null, action) => {
+  switch (action.type) {
+    case GET_SELECT_MOVIE:
+      return action.payload.data;
+    default:
+      return selectMovie;
+  }
+}
 
 const rootReducer = combineReducers({
   movies: MoviesReducer,
-  selectMovie: SelectMovieReducer
+  selectMovie: GetSelectMovieReducer
 });
 
 export default rootReducer;
