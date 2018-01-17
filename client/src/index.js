5c93a040004dca94f686d7d1c4bc70e2302883cd
unknown <brcampbell001@outlook.com>
2018-01-17 09:38:57 -0600

    Ben Campbell - Router Sprint: Progress not working right yet

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,15 @@ import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 ReactDOM.render(
-  <div>Application running add your routing</div>,
+  <Router>
+  <div className="movtitle">
+    <h1>Movies</h1>
+    <Route path="/" component={MovieList} />
+    <Route path="/Movie/:id" component={Movie} />
+  </div>
+  </Router>,
   document.getElementById('root')
 );
