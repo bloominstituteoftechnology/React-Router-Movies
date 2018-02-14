8750e7bef2172229c2fa99f8817f5c4e8dff2d7f
Res Yeg <shaqk702@gmail.com>
2018-02-14 12:08:55 -0800

    finished install runs

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+function 
+
 ReactDOM.render(
   <div>Application running, add your routing</div>,
   document.getElementById('root')
 );
+
