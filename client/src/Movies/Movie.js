d2e3a3f4fc06d9a2ef208c1cb624bad1fadf13bb
Eric <erichech@gmail.com>
2018-02-14 18:02:18 -0500

    Finished assignment.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import "./Movie.css";
 
 export default class MovieCard extends React.Component {
-  
   state = {
     movie: null,
   };
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -19,13 +20,16 @@ export default class MovieCard extends React.Component {
   }
 
   render() {
-    if(!this.state.movie) {
-      return <div>Loading movie information...</div>
+    if (!this.state.movie) {
+      return <div>Loading movie information...</div>;
     }
 
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="movie-card">
+        <NavLink to="/">
+          <div className="home-button">Return Home</div>
+        </NavLink>
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
