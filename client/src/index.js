cfc83cb162b53b70146aa77ab40cd7815e23f509
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-14 16:21:36 -0500

    Gotlinks to individual Movie cards working

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,10 @@ import Movie from './Movies/Movie'
 ReactDOM.render(
   <Router>
     <div>
-      <NavLink to="/" activeClassName="navlink--active"><MovieList /></NavLink>
-      <route path="/" component={MovieList}/>
-      <route path="/movies/:id" component={Movie} />
+      {/* <NavLink to="/" activeClassName="navlink--active">MovieList</NavLink> */}
+      {/* <NavLink to="/movies/:id" activeClassName="navlink--active">Movie</NavLink> */}
+      <Route path="/" component={MovieList} exact />
+      <Route path="/movies/:id" component={Movie} exact />
     </div>
   </Router>,
   document.getElementById('root')
