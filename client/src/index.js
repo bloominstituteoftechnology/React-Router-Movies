c2d93d18ebdbe75500701f6c07e36bc795a8eaeb
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-14 15:09:11 -0500

    First Commit/Movie List
    index.js --- Added Route/Link.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+    <div>
+        <li>
+          <NavLink to="/movielist">Movie List</NavLink>
+        </li>
+      <Route path="/movielist" component={MovieList} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
