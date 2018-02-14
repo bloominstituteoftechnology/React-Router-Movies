3c9f1ebd432ddff9a5c9058da18867aca77bc381
Igor Yermak <igor.yermak@gmail.com>
2018-02-14 15:57:59 -0500

    Igor Yermak -WIP
    Added routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink
+} from 'react-router-dom'
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
-ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+function Home() {
+  <div>
+    <MovieList />
+    <Movie/>
+  </div>
+}
+
+
+
+
+
+  ReactDOM.render(
+
+
+  <Router>
+    <div>
+    <Route path="/" component={MovieList} />
+    <Route path="/movies#" component={Movie} />
+    </div>
+  </Router>, document.getElementById('root')
 );
