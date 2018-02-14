a3322004cae52983fc9a27de976b74686ab98480
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-02-14 15:33:52 -0500

    Initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+    <div>Application running, add your routing</div>
+  </Router>,
   document.getElementById('root')
 );
