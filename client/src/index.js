ac5c1900a827693d6decfb870baa3c2403291608
Amanda Phillips <amanda@phillipsdev.org>
2018-02-14 15:10:38 -0600

    Initial commit, movie list router add

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+  <div>
+    <ul>
+        <li>
+          <NavLink to="/api/movies">
+            Movie List
+          </NavLink>
+        </li>
+      </ul>
+    <Route path="/api/movies" component={MovieList}/>
+  </div>
+  </Router>,
   document.getElementById('root')
 );
