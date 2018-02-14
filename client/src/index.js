0001993db07fc2823c6d51a3b5030f41fbfd87fe
Dan-Lara <endtrippy@gmail.com>
2018-02-14 13:35:22 -0700

    Imported Browser router and other router library tools / set render to use Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+// function MovieList(props) {
+//   return ()
+// }
+
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+  <div>Application running, add your routing</div>
+  </Router>,
   document.getElementById('root')
 );
