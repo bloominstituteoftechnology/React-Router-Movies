8b72b7ab51e401e26a931981f9712188811d74c8
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-02-14 13:11:07 -0800

    <Leon> Finished assignment.
    --Added route for individual movies to index.js @ line 15.  Simply leaving off the `exact` will allow each component to serve up its own id to the url and still be found with no trouble.
    --Added link back home to `MovieCard` component in Movie.js @ 45-47.  Note - you have to import `Router` as well as `Link`, or the stack frame crashes. Not sure why...
    --Added logic to `MovieCard` component in Movie.js @ 13-14.  Now, when the MovieCard component is loaded, it parses the URL of the page in order to find the ID that corresponds to the selected film.  Then, it can pass that ID to its `get` call to retrieve the correct data from the server.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 export default class MovieCard extends React.Component {
   
@@ -9,7 +10,8 @@ export default class MovieCard extends React.Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const path = this.props.location.pathname;
+    const id = parseInt(path.substr(8));
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -40,6 +42,9 @@ export default class MovieCard extends React.Component {
             {star}
           </div>
         ))}
+        <Link to="/">
+          Home
+        </Link>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
@@ -12,7 +12,7 @@ ReactDOM.render(
   <Router>
     <div>
       <Route path="/" component={MovieList} exact/>
-      <Route path={`/movies/1`} component={Movie}/>
+      <Route path="/movies/" component={Movie}/>
     </div>
   </Router>,
   document.getElementById('root')
