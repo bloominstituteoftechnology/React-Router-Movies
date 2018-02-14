15c30aa961ba74d2e2e395d1aa025d9ce323d973
german gonzalez <gcomlnk@gmail.com>
2018-02-14 15:46:22 -0500

    addign routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
+
+function Home() {
+    return (
+        <div>
+            <MovieList/>
+        </div>
+    )
+}
+
+function SingleMovie(props) {
+    return (
+        <div>
+            <Movie id={props.match.params.id}/>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+
+        <Router>
+            <div>
+                <NavLink to="/" activeClassName="back-lnk" exact> Back </NavLink>
+
+                <Route path="/" component={Home} exact />
+                <Route path="/movies/:id/"  component={SingleMovie}/>
+            </div>
+        </Router>
+    , document.getElementById('root')
+
 );
