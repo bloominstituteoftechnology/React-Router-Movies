a8e15e55e79ed55684db831fb932f7a4ce153a33
Glenn-David Daniel <d2rd@outlook.com>
2018-02-14 16:01:23 -0800

    WIP cards working need to enable links

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,48 @@ import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+import {BrowserRouter as Router, Route, NavLink } from 'react-router-dom'; // new
+
+function About() {
+  return (
+    <div>
+      <h1>About component</h1>
+      <p id="paragraph">This is the about component</p>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home component</h1>
+      <p id="paragraph">This is the home component</p>
+    </div>
+  );
+}
+
+
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  
+  <Router>
+  <div>
+    <ul>
+      <li>
+        <NavLink to="/" activeClassName="navlink--active" exact>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about" activeClassName="navlink--active">About</NavLink>
+      </li>
+      {/* <li>
+        <link to="/products">Products</link>
+      </li> */}
+    </ul>
+    <Route path='/' component={MovieList} exact />
+    <Route path="/about" component={Movie} />
+    {/* <Route path="/products" component={Products} /> */}
+  </div>
+</Router>,
+
   document.getElementById('root')
 );
+
