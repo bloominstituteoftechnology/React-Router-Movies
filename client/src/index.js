0b6e8fc4c9346144f8812ad8f6a44b17bf471327
liz <elizabethbraggisgreat@gmail.com>
2018-03-12 16:33:56 -0400

    installed and imported router into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+  
+  </Router>
+  , document.getElementById('root')
 );
