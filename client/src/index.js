132c533759387e9200eac2dbc4df14ac6948bf3e
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-03-12 15:43:41 -0700

    finished :)

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
+import './index.css';
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <div>
+    <Router>
+      <div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
+      </div>
+    </Router>
+  </div>,
   document.getElementById('root')
 );
