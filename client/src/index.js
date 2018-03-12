7337e653839f73246ebe11d44550503e7e89036b
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-12 12:52:26 -0500

    Shows Movies
    Need to create a dynamic ID.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route path = "/" component = { MovieList } key = {Movie.id} />
+      <Route path = {Movie.id} component = { Movie }  />
+    </div>
+  </Router>
+  ,document.getElementById('root')
 );
