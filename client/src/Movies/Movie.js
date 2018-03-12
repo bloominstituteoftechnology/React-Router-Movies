f08f22e0c1ba8d7ae4a7edbb3101d054aaf5f5b1
kait-schorr <kaitlynschorr@gmail.com>
2018-03-12 14:26:09 -0500

    Linked the moviecards with dynamic indexes and added some styling

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class MovieCard extends React.Component {
-  
   state = {
-    movie: null,
+    movie: null
   };
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.movieId;
+    console.log("Props", this.props.match.params.movieId);
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -19,27 +20,32 @@ export default class MovieCard extends React.Component {
   }
 
   render() {
-    if(!this.state.movie) {
-      return <div>Loading movie information...</div>
+    if (!this.state.movie) {
+      return <div>Loading movie information...</div>;
     }
 
     const { title, director, metascore, stars } = this.state.movie;
     return (
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
+      <div>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
           </div>
-        ))}
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
+        <Link to="/" className="home-button">
+          Home
+        </Link>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 // PR comment
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './index.css';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import "./index.css";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:movieId" component={Movie} />
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
