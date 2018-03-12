553af216d7a6042e3b0d61abddfb31117673eed9
Jeremy Jones <jeremyjones@Macintosh-58b035f2f989.local>
2018-03-12 13:20:12 -0700

    Completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
 
-ReactDOM.render(
-  <div>Application running, add your routing</div>,
+const App = () => (
+  <div>
+    <Navigation />
+    <Route exact path="/" component={MovieList} />
+    <Route path="/movies/:id" component={Movie} />
+  </div>
+);
+
+const Navigation = () => {
+  return (
+    <div className="home">
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+render(
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
