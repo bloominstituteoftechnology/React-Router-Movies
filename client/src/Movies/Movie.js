ff17762687e7b720426b6d7318f2f95adfd4470a
Cesar Mejia Leiva <cmejia@gmail.com>
2018-03-12 14:43:08 -0700

    project complete; this project was lacking in substance

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieCard extends React.Component {
   
@@ -7,9 +8,10 @@ export default class MovieCard extends React.Component {
     movie: null,
   };
 
-  componentDidMount() {
+  componentDidMount(  ) {
     // change this line to grab the id passed on the URL
-    const id = 1;
+    console.log(this.props.match)
+    const id = this.props.match.params.id;
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
@@ -40,6 +42,7 @@ export default class MovieCard extends React.Component {
             {star}
           </div>
         ))}
+        <Link to="/"><p>Return to Movie List</p></Link>
       </div>
     );
   }
