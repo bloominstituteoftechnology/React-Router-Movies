d60688ea759cb3b0aaadcb465cd569d24ed6b6c0
Juber Ahmed <a.juber@hotmail.com>
2018-03-12 16:53:43 -0400

    Added Routes: Fully Functioning

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 import "./index.css";
 import MovieList from "./Movies/MovieList";
@@ -10,9 +10,9 @@ import Movie from "./Movies/Movie";
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path="/" component= { MovieList }/>
-      <Route path="/:movies/id" component= { Movie } />
-    </div> 
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
+    </div>
   </Router>,
   document.getElementById("root")
 );
