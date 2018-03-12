8feb4fcf22e0c2bea0f11dd46d81660ab896ab41
unknown <jacobiswinning@gmail.com>
2018-03-12 15:58:10 -0600

    Jake Henry Client-side-routing

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import axios from 'axios';
+import {
+  Link
+} from 'react-router-dom';
 
 export default class MovieCard extends React.Component {
-  
+
   state = {
     movie: null,
   };
 
   componentDidMount() {
+
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => this.setState(() => ({ movie: response.data })))
+      .then(response => this.setState(() => ({
+        movie: response.data
+      })))
       .catch(error => {
         console.error(error);
       });
   }
 
   render() {
-    if(!this.state.movie) {
-      return <div>Loading movie information...</div>
+    if (!this.state.movie) {
+      return <div> Loading movie information... </div>
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
+    const {
+      title,
+      director,
+      metascore,
+      stars
+    } = this.state.movie;
+
+    return ( 
       <div className="movie-card">
-        <h2>{title}</h2>
+        <Link to="/"> Back to list </Link> 
+          <h2> {title} </h2>
         <div className="movie-director">
-          Director: <em>{director}</em>
+          Director: <em> {director} </em>
         </div>
         <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
+          Metascore: < strong > {metascore} </strong>
         </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+          <h3> Actors </h3>
+          {stars.map(star => (
+        <div key = {star}
+              className = "movie-star">{star}
+        </div>
+            ))
+          }
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/" component={Movie} />
+    </div>
+  </Router>
+  , document.getElementById('root')
 );
