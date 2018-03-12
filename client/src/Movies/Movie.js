e8e2ccb1ef62ad306889c0ae13355f6814dba054
Charletta <charlettabullard12@outlook.com>
2018-03-12 19:21:54 -0400

    I think I fixed my issue but I am having tech issues. Will try again

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieCard extends React.Component {
   
@@ -10,9 +11,10 @@ export default class MovieCard extends React.Component {
   componentDidMount() {
 
     // change this line to grab the id passed on the URL
-    const id = this.props.movie.id;
+    const id = this.props.movie;
+    const { match: { params } } = this.props;
     axios
-      .get(`http://localhost:5000/api/movies/${id}`)
+      .get(`http://localhost:5000/api/movies/${params.id}`)
       .then(response => this.setState(() => ({ movie: response.data })))
       .catch(error => {
         console.error(error);
@@ -26,22 +28,24 @@ export default class MovieCard extends React.Component {
 
     const { title, director, metascore, stars } = this.state.movie;
     return (
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
+      <Link>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
           </div>
-        ))}
-      </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
+      </Link>
     );
   }
 }
