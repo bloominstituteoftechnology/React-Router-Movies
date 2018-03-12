243bcfdd5b08d2c34ec8924ca20b23ba77f0faab
Joshua Goode <jgoode.business@gmail.com>
2018-03-12 19:22:10 -0400

    Removed director, metascore, stars from MovieList

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -17,7 +17,7 @@ export default class MovieList extends React.Component {
         console.error('Server Error', error);
       });
   }
-  F;
+  
 
   render() {
     return (
@@ -36,19 +36,6 @@ function MovieCard({ movie }) {
     <Link to={`/movies/${movie.id}`}>
       <div className="movie-card">
         <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
       </div>
     </Link>
   );
