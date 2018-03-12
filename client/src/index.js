c4296aa9bc642bfefb02c9d718d0e3453e0a5805
Kenny <khpham24@gmail.com>
2018-03-12 15:06:20 -0600

    Kenny
    finished(I think) npm start wouldnt work for the life of me soo...

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './index.css';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import "./index.css";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+const App = () => (
+  <div>
+    <Route exact path="/" component={MovieList} />
+    <Route path="/movies/:id" component={Movie} />
+  </div>
+);
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
