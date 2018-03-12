031f2f6f001d7ca0187d23d7934cca0e3c97b265
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-12 15:57:33 -0500

    Passes ID

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
@@ -10,8 +9,8 @@ import Movie from './Movies/Movie';
 ReactDOM.render(
   <Router>
     <div>
-      <Route path = "/" component = { MovieList } key = {Movie.id} />
-      <Route path = {Movie.id} component = { Movie }  />
+      <Route path = "/" component = { MovieList } />
+      <Route path = "/movies/:id" component = { Movie } />
     </div>
   </Router>
   ,document.getElementById('root')
