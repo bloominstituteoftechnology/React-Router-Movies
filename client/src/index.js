c0e9be6a6a40f750e8a0ef134f3752a9d803db1e
mjd02 <mostdarryl@gmail.com>
2018-03-13 06:51:37 -0400

    forgot to push index

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
+  <Router>
+  <div>
+    <Route exact path="/" component={MovieList} />
+    <Route path="/movies/:id" component={Movie} />
+  </div>
+
+  </Router>,
   document.getElementById('root')
 );
