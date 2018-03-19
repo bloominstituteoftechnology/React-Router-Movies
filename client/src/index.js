3e378bac8cbdf683ea94cef8dfa0d91f3571ca8a
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-19 00:37:06 -0400

    made a typo, always, fixed and now it works!  good gracious almighty

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
 
 ReactDOM.render(
   <Router>
    <div>
-    <Route exact path="/" component={ MovieList }></Route>
-    <Route exact path="/movie/:id" component={ Movie }></Route>
+    <Route exact path='/' component={ MovieList }></Route>
+    <Route exact path='/movies/:id' component={ Movie }></Route>
    </div>
-  </Router>,
-  document.getElementById('root')
-);
+  </Router>
+  , document.getElementById('root'));
