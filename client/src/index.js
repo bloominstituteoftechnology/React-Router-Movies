6779fc0081ee885ebbd1ebe0748ef89cca0fa422
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-03-19 16:02:35 -0500

    Jonathan Reeves WIP Client-Side-Routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
@@ -7,5 +8,7 @@ import Movie from './Movies/Movie'
 
 ReactDOM.render(
   <div>Application running, add your routing</div>,
+  <Route path="/" component={ MovieList} />,
+  <Route path='/movielist/:id' component={ Movie} />,
   document.getElementById('root')
 );
