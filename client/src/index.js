e2ca6cba43417dfe617c58912f62644c0ba84fbb
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-18 23:45:55 -0400

    adding routes to index.js, completely so it works for realzies

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Route
-}
+} from 'react-router-dom';
 
 import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
-  <div>
   <Router>
-  <Route exact path="/" component={ MovieList }></Route>
-  <Route exact path="/movie/:id" component={ Movie }></Route>
-  </Router>
-  </div>,
+   <div>
+    <Route exact path="/" component={ MovieList }></Route>
+    <Route exact path="/movie/:id" component={ Movie }></Route>
+   </div>
+  </Router>,
   document.getElementById('root')
 );
