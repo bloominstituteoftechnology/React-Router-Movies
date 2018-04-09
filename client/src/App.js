5f59955809241a38f52745860a5c388d9fa86cae
totsuya <supertatortots@gmail.com>
2018-04-09 15:27:48 -0400

    added second route

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
+        <Route path="/Movies:name" component={Movie} />
       </div>
     );
   }
