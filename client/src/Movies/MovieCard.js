6de020348dc4c42ed5144b192da7127f35bfc716
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-09 17:36:06 -0400

    David: i made changes to try and figure out part 2

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return (
+    <div>
+      <Nav className="App">
+        <h1>React Router Mini</h1>
+        <NavItem>
+          <Link to="/movies/id">MovieList</Link>
+        </NavItem>
+      </Nav>
+    </div>
+  )
 };
 
 export default MovieCard;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard'
 
 export default class MovieList extends Component {
   constructor(props) {
