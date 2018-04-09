877e075ffd64445c258a06181077c8cd0b19ea53
Jonathan Greene <greeneselectronics@gmail.com>
2018-04-09 15:20:39 -0600

    Completed the project as a team

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import {Route} from 'react-router-dom';
+import {Link}from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -22,7 +23,11 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path='/' component={MovieList}/>
+        <Route path='/movies/:id' component={Movie}/>
+        
+        {/* <div><Link to="/"> Movie List </Link></div>
+        <div><Link to="/movie/:id"> Movie </Link></div> */}
       </div>
     );
   }
