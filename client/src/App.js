759929314daebf1cd08438c74c185f5a72caf59c
imsdhk <jappleleaf@gmail.com>
2018-04-09 17:59:05 -0400

    removed comments

--- client/src/App.js
+++ client/src/App.js
@@ -31,25 +31,3 @@ export default class App extends Component {
     );
   }
 }
-
-
-// const Navigation = () => {
-//   return (
-//     <div>
-//       <Nav className="App">
-//         <h1>React Router Mini</h1>
-//         <NavItem>
-//           <Link to="/">Home</Link>
-//         </NavItem>
-//         <NavItem>
-//           <Link to="/about">About</Link>
-//         </NavItem>
-//         <NavItem>
-//           <Link to="/contact">Contact</Link>
-//         </NavItem>
-//       </Nav>
-//     </div>
-//   );
-// };
-
-// export default Navigation;
