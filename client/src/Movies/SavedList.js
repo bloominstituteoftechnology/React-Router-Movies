6a53f6368fc42fd5bfff227c009ac5b35be6f989
Axhon Ruiz <axhonruiz+github@gmail.com>
2018-04-09 12:52:20 -1000

    removed console.log

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -9,7 +9,6 @@ export default class SavedList extends Component {
   render() {
     return (
       <div className="saved-list">
-        {console.log(this.props)}
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
