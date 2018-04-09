20209ddbeeb5b893648f68e18680c3ae0c7cbee7
Devon Bacon <devon.bacon@gmail.com>
2018-04-09 12:48:26 -0700

    Set up imports and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -18,11 +19,19 @@ export default class App extends Component {
     this.setState({ savedList });
   };
 
+
+  // const Movies = ({match}) => {
+  //   return (
+  //     <div> {match.params.movie}</div>
+  //   )
+  // }
+
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
