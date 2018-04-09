fc568dba4e597f2d90508b0d4d84059fe4414e6a
cgoez <goez2001@gmail.com>
2018-04-09 14:07:14 -0700

    added Justin Rigler commits to working repo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import { BrowserRouter as Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -22,11 +23,12 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-      {/* path to a list of movies*/}
+        {/* path to a list of movies*/}
         <Route exact path="/" component={MovieList} />
-      {/* path to individual movies */}
+        {/* path to individual movies */}
         <Route path="/movies/:id" component={Movie} />
       </div>
+      // <MovieDetails />
     );
   }
 }
