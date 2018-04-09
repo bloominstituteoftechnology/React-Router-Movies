a0972c05ddb56bc8992092ecb2c6d361e5538b45
Bee C <b.choeung@gmail.com>
2018-04-09 17:27:37 -0400

    completed step1 and step2 with my team, will work on stretch goals tomorrow

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
-
+// import { Home, MovieList } from './movies';
+import { Route } from 'react-router-dom';
 export default class App extends Component {
   constructor() {
     super();
@@ -22,7 +23,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/Movies/:id" component={Movie} />
       </div>
     );
   }
