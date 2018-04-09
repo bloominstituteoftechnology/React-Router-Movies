dbe2ca55da3cf679275f8f0668c4199b86bd9ee5
Boredom <macnwandu@gmail.com>
2018-04-09 15:23:30 -0500

    have links working

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>
-          <Route path="/" component={MovieList} />
-          <Route path="/movies/:movie.id" component={Movie} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
         </div>
       </div>
     );
