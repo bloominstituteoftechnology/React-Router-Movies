1f394ce0af85e7d412f20e2981907b41547cd1e2
Henry K <mykim1891@hotmail.com>
2018-04-09 16:17:40 -0700

    Last Stretch goal left; everything else done.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { Route } from "react-router-dom";
-
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -13,9 +11,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span key={movie.id} className="saved-movie">
-            <Link to=`/movies/${movie.id}`>{movie.title}</Link>
+          <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <span className="saved-movie">
+            {movie.title}
           </span>
+          </Link>
         ))}
         <div className="home-button">
           <Link to="/">Home</Link>
