7bb29dc54c6955d92e2192bd3a55e3decd54665b
Justin M <jekm321@gmail.com>
2018-04-09 17:30:54 -0400

    refractered for moviecard

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 import axios from 'axios';
 
 export default class MovieList extends Component {
@@ -28,33 +29,11 @@ export default class MovieList extends Component {
         {this.state.movies.map(movie => (
           <div key={movie.id}>
             <Link to={`/movies/${movie.id}`}>
-              <MovieDetails key={movie.id} movie={movie} />
+              <MovieCard key={movie.id} movie={movie} />
             </Link>
           </div>
         ))}
       </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
 }
