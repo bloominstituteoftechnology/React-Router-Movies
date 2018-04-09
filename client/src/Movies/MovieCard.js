60048ffcc9400952cbe891372aae74203044181e
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-09 17:05:01 -0400

    DRY

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
-};
+export default class MovieCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: this.props
+    };
+  }
 
-export default MovieCard;
+  render() {
+    const movieRef = this.state.movie.movie;
+    return (
+      <Link to={`/movies/${this.state.movie.id}`}>
+        <div className="movie-card">
+          <h2>{movieRef.title}</h2>
+          <div className="movie-director">
+            Director: <em>{movieRef.director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{movieRef.metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+          {movieRef.stars.map((star, index) => (
+            <div className="movie-star" key={star + index}>{star}</div>
+          ))}
+        </div>
+      </Link>
+    )
+  }
+}
