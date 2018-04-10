8dbdbf6c42a7d388beb538aea00484adf94b1677
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-10 16:29:02 -0400

    fix that got this working again

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -13,7 +13,7 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <Link className="home-button" to="/">Homes</Link>
+        <Link className="home-button" to="/">Home</Link>
       </div>
     );
   }
