82dbbf721866f95bf3f3269de9403b91b042ff44
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-10 12:40:07 -0700

    got the save movie function to work

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -27,11 +27,11 @@ export default class Movie extends Component {
       });
   };
   //Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
@@ -44,13 +44,11 @@ export default class Movie extends Component {
     }
 
     const { movie } = this.state;
-    return( 
-      <div className="save-wrapper">
+    return <div className="save-wrapper">
         <MovieCard movie={movie} />
-        <div className="save-button">
+        <div className="save-button" onClick={() => this.saveMovie()}>
           Save
         </div>
-      </div>
-    );
+      </div>;
   }
 }
