aff57a01d926346bd9ebbb9a21ce57b3f7826756
Henry K <mykim1891@hotmail.com>
2018-04-09 17:32:08 -0700

    installed reactstrap, fixed yarn issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-
 import { Route } from "react-router-dom";
 
 export default class App extends Component {
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,11 +12,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} key={movie.id}>
-          <span className="saved-movie">
-            {movie.title}
-          </span>
-          </Link>
+          <Nav key={movie.id} className="saved-movie">
+            <NavItem>
+              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </NavItem>
+          </Nav>
         ))}
         <div className="home-button">
           <Link to="/">Home</Link>
