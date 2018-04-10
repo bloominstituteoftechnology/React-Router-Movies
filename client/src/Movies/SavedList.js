c355027d69b645ebc612b9047c20d219afb7d528
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-10 19:17:47 -0400

    Robert Hogan. Further Progression on streatch goals.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -7,11 +7,14 @@ export default class SavedList extends Component {
   }
 
   render() {
+    console.log(this.props.list);
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map((title, index) => (
-          <span className="saved-movie" key={index}>{title}</span>
+        {this.props.list.map((movie, index) => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <span className="saved-movie" key={index}>{movie}</span>
+          </Link>
         ))}
         <Link to={`/`}>
           <div className="home-button">Home</div>
