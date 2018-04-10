da34aa583ff6fd9769c8a1cd3c53d84ee42f645e
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-09 17:33:14 -0700

    started have route imported, have a component to hold my navigation bar next step is to add functionality to the nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import NavBar from './nav'
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -23,6 +24,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route exact path="/" />
+        <NavBar />
       </div>
     );
   }
---
+++ client/src/nav.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Nav, NavItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+          <Nav>
+        {this.state.movies.map(movie => (
+            <NavItem>
+          <Link to={'/Movie/' + movie.id}>{movie.title}</Link>
+          <Route path={'/Movie/' + movie.id} />
+            </NavItem>
+        ))}
+         </Nav>
+      </div>
+    );
+  }
+}
