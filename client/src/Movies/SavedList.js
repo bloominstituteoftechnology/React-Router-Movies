37d1dac8ad92f70b9983af4b5d10e7ef8d824687
nickbrenn <nbren@tutanota.com>
2018-04-10 16:37:35 -0400

    Started working on making links in save list work

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
   }
 
   render() {
@@ -11,9 +27,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map((title, index) => (
-          <span className="saved-movie" key={index}>
-            {title}
-          </span>
+          <Link key={index} to={`/movies/${index}`}>
+            <span className="saved-movie" key={index}>
+              {title}
+            </span>
+          </Link>
         ))}
         <Link to={`/`}>
           <div className="home-button">Home</div>
