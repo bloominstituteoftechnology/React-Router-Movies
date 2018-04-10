8afa374a204c0454cebfe12a96663d362fe92f1b
Justin Hong <jhong2009@gmail.com>
2018-04-10 12:53:38 -0700

    added a Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import { Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -24,9 +24,13 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         {/* path to a list of movies*/}
-        <Route exact path="/" component={MovieList} />
-        {/* path to individual movies */}
-        <Route path="/movies/:id" component={Movie} />
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          {/* path to individual movies */}
+          <Route path="/movies/:id" component={Movie} />
+          {/* Default */}
+          <Route component={() => <h1>Invalid URL! GO BACK HOME</h1>} />
+        </Switch>
       </div>
     );
   }
