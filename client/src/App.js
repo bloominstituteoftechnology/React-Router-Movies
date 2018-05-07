752c898608e808f74462dd1362de7e7514dbda5e
jesuarva <jean.ariza@jesuarva.com>
2018-05-08 00:56:34 +0200

    IMPROVE addTosavedList function
    DONE: do not allow duplicated movies in the App.state.savedList.
    Do not allow to add a Movie that was already added to the list.

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,12 @@ export default class App extends Component {
   addToSavedList = movie => {
     console.log("addToSavedList invoked");
     const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+    if( this.state.savedList.filter( savedMovie => savedMovie.id === movie.id ).length === 0 ) {
+      console.log(this.state.savedList.filter( savedMovie => savedMovie.id === movie.id ))
+      savedList.push(movie);
+      this.setState({ savedList });
+      console.log(this.state.savedList.filter( savedMovie => savedMovie.id === movie.id ))
+    }
   };
 
   render() {
@@ -26,12 +30,16 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
+
         {/* <div>Replace this Div with your Routes</div> */}
         <Route exact path="/" component={MovieList} />
+        
         {/* <Route path="/movies/:id" component={Movie} /> */}
+        {/* <Route path="/movies/:id" render={(props) => <Movie routeProp={props} save={this.addToSavedList} /> } /> */}
         <Route path="/movies/:id" render={ props => (
-          <Movie {...props} addToSavedList={addToSavedList} />
+          <Movie {...props} addToSavedList={addToSavedList}  />
         ) } />
+        
       </div>
     );
   }
