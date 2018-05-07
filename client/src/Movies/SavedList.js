8b4929628f6fa7a9068822c5a9ecca6a85515121
Thuy Pham <tpham045@hotmail.com>
2018-05-07 12:19:59 -0700

    feat: change movies in SavedList to NavLink

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,11 +12,18 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link key={movie.id} to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: 'inherit'}}>
+          <NavLink 
+            key={movie.id} 
+            to={`/movies/${movie.id}`} 
+            style={{textDecoration: 'none', color: 'inherit'}}
+            activeStyle={{
+              fontWeight: 'bold'
+             }}
+          >
             <span className="saved-movie">
               {movie.title}
             </span>
-          </Link>
+          </NavLink>
         ))}
         <Link to='/' style={{textDecoration: 'none', color: 'inherit'}}><div className="home-button">Home</div></Link>
       </div>
