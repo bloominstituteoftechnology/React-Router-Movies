32d4726a3727db800718a1c92315c3f12037448d
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-07 13:07:03 -0700

    stretch - make code DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class Movie extends Component {
   constructor(props) {
@@ -41,10 +42,10 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    //const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
+        {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
             Director: <em>{director}</em>
@@ -59,7 +60,8 @@ export default class Movie extends Component {
               {star}
             </div>
           ))}
-        </div>
+        </div> */}
+        <MovieCard movie={this.state.movie}/>
         <div className="save-button">Save</div>
       </div>
     );
