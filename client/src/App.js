0aaccea01be40b4a0079d009f128e15c984f8a48
Chris Honda <christopher.honda92@gmail.com>
2018-05-07 13:57:51 -0700

    Working on part 2: movies render, links are active, but links donly render loading movie text

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>
-          <Route path="/" component={MovieList}></Route>
+          <Route exact path="/" component={MovieList}></Route>
           <Route path="/movies/:id" component={Movie}></Route>
         </div>
       </div>
