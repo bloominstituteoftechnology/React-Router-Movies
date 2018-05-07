b8c0e09650321a2d81f7c4cdf3e128f7bbc1175c
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-05-07 15:53:52 -0400

    Eddy updates v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
-
+import { Route } from 'react-router-dom'; 
+ 
 export default class App extends Component {
   constructor() {
     super();
@@ -22,7 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        {/* <div>Replace this Div with your Routes</div> */}
+        <Route exact path="/" component={}></Route> 
       </div>
     );
   }
