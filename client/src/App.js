42b1c71a6bb672c035639de5a184a575cb7112ef
Tramane Hall <tramanedhall@gmail.com>
2018-05-07 18:37:13 -0400

    Feature Finished: Home Button redirects to Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+
 export default class App extends Component {
   constructor() {
     super();
@@ -22,8 +23,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />   
-        <Route path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} /> 
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={Movie} /> 
       </div>
     );
   }
