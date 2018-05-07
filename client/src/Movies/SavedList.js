15920ffc593d8637837861891ec7efb76cbe54c8
Thuy Pham <tpham045@hotmail.com>
2018-05-07 12:15:56 -0700

    feat: Turn all movies in SavedList into Links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -12,7 +12,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span key={movie.id} className="saved-movie">{movie.title}</span>
+          <Link key={movie.id} to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: 'inherit'}}>
+            <span className="saved-movie">
+              {movie.title}
+            </span>
+          </Link>
         ))}
         <Link to='/' style={{textDecoration: 'none', color: 'inherit'}}><div className="home-button">Home</div></Link>
       </div>
