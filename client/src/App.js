704580d7d153ff35dd8c7f7246fc7cba71859932
Tramane Hall <tramanedhall@gmail.com>
2018-05-07 15:50:37 -0400

    Feature Added: Added MovieList & Movie Routes

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <div>
+          <Route path="/" component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
+        </div> 
       </div>
     );
   }
