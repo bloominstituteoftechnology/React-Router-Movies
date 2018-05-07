fab2306778d848a49e206f39ed76c7451334acd3
masontmorrow <masontmorrow@gmail.com>
2018-05-08 01:55:16 +0200

    Added MovieCard.js
    - DRY implementation of the MovieCard component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const MovieCard = () => {
-  <div>
-    {MovieList.state.movies.map(movie => {
-      return (
-        <Link to={`/movies/${movie.toLowerCase()}`} key={movie}>{movie}</Link>
+const MovieCard = (props) => {
+  return (
+    <div className="movie-card">
+      <h2>{this.title}</h2>
+      <div className="movie-director">
+        Director: <em>{this.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{this.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
       );
-    })}
-  </div>
 };
 
 export default MovieCard;
