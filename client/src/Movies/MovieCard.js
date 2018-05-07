93b222ae6558f484a31c0e964b28c59c19f09230
Bee C <b.choeung@gmail.com>
2018-05-07 16:57:51 -0400

    wrapped app in router and displayed movieslist

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return (
+  <div>
+    {MovieList.map(movie => <Link to={`/MovieList/${movie}`} key={movie}>{movie}</Link>)}
+  </div>)
+
 };
 
 export default MovieCard;
