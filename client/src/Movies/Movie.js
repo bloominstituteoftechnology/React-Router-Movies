9a3ab76e864f8e952aabec82befd935f72461c96
Brad Traversy <mercado7946@gmail.com>
2018-05-07 14:33:13 -0700

    added functionality to the home button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,6 +12,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
+    console.log(this.props);
     const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
