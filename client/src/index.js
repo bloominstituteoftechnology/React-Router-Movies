99b99e0052f047df980e8ac399d9c0fca32b01a8
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-07 15:01:57 -0400

    declared routes or MoveList and Movie in the App.js  and imported Route

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+
 import './index.css';
 import App from './App';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+// const App = () => (
+//     <div>
+//         <Route path="/" component={MovieList} />
+//         <Route path="/movies/:id" component={Movie}/>
+//     </div>
+// );
 
 
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root'));
