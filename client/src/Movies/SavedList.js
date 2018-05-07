ea1146e0db61b93de7df1f50c908a6809a17768f
Zach Campbell <beazlebubbles@gmail.com>
2018-05-07 17:28:44 -0600

    Refactored SavedList to be a functional component

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import './SavedList.css';
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <NavLink to={`/movies/${movie.id}`} className="saved-movie" activeClassName="selected-movie">{movie.title}</NavLink>
-        ))}
-        <Link to="/">
-          <div className="home-button">Home</div>
-        </Link>
-      </div>
-    );
-  }
+const SavedList = (props) => {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <NavLink to={`/movies/${movie.id}`} className="saved-movie" activeClassName="selected-movie">{movie.title}</NavLink>
+      ))}
+      <Link to="/">
+        <div className="home-button">Home</div>
+      </Link>
+    </div>
+  );
 }
+
+export default SavedList;
