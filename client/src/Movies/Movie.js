b40609fb0e2b093704c8d2be40cf655bbc6d42b6
NandoTheessen <nando.theessen@gmail.com>
2018-05-07 21:12:45 +0200

    Finished task 1 +2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,8 +11,8 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
-    this.fetchMovie(id);
+    console.log("props", this.props.match.params.id)
+    this.fetchMovie(this.props.match.params.id);
   }
 
   fetchMovie = id => {
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -32,9 +33,11 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
+  // console.log("ID", movie.id)
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${movie.id}`}>
+      <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -50,5 +53,6 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </Link>
   );
 }
