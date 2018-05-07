ca9e5753efe1cd07859076b782ede2fc20665afc
Zach Campbell <beazlebubbles@gmail.com>
2018-05-07 17:17:29 -0600

    Refactored to use MovieCard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from "./MovieCard.js";
 
 export default class Movie extends Component {
   constructor(props) {
     super(props);
-    console.log(props.misterProp);
     this.state = {
       movie: null,
       match: props.match
@@ -37,33 +37,18 @@ export default class Movie extends Component {
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
     addToSavedList(this.state.movie)
+    console.log("wut");
   }
 
   render() {
     if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
+      return <div> Loading movie information...  </div>;
     }
 
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button" onClick={this.saveMovie} >Save</div>
+        <MovieCard title={title} director={director} metascore={metascore} stars={stars} saveMovie={this.saveMovie} />
       </div>
     );
   }
