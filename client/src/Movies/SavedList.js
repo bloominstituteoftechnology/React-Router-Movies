2f850b97f8de4888427c26c400411a17d91c105e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-07 15:22:20 -0700

    save func added, strect goal

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
   }
-
   render() {
+    console.log(this.props)
     return <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <Link exact to="/" className="home-button">Home</Link>
+        {this.props.list.map(movie => <Link to={`/movies/${movie.id}`}>
+            <span className="saved-movie">{movie.title}</span>
+          </Link>)}
+        <Link exact to="/" className="home-button">
+          Home
+        </Link>
       </div>;
   }
 }
