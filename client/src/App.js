ce61826b3b89bd9f0316c498d8f7305d665e7f4e
kelly <kelfro@fastmail.com>
2018-05-07 12:51:51 -0700

    working on Part 2
    Lots of moving parts - it's not working now, and I don't think it's going to until I'm totally done.

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
 	<Route exact path="/" component={MovieList}></Route>
-        <Route path="/movie:id" component={Movie}></Route>
+        <Route path="/movies/:id" component={Movie}></Route>
       </div>
     );
   }
