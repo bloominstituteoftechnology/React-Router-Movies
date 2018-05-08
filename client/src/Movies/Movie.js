1bed87b963fb5f8cd296bbbce75d7e7696f160c4
Bradiowave <divebombwalker@gmail.com>
2018-05-08 14:07:13 -0700

    actually added save movie functionality to Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -46,7 +46,7 @@ export default class Movie extends Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.props.addToSavedList}>Save</div>
+        <div className="save-button" onClick={() => this.props.addToSavedList(this.state.movie)}>Save</div>
       </div>
     );
   }
