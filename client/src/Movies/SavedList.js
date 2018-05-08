f1040dc4e139ae0806a6d8b7212e9d1f26e270cc
Bee C <b.choeung@gmail.com>
2018-05-08 17:04:55 -0400

    refactored code to make it dry

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -11,8 +11,9 @@ export default class SavedList extends Component {
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
-        ))}
+        ))}<Link to="/" >
         <div className="home-button">Home</div>
+        </Link>
       </div>
     );
   }
