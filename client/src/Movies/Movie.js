e25b6356b67147cd29f7a0504981693883ed5dcf
William VanDolah <william.vandolah@gmail.com>
2018-05-08 13:36:56 -0700

    Created MovieCard component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard'
 
 export default class Movie extends Component {
   constructor(props) {
@@ -42,10 +43,12 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    // const { title, director, metascore, stars } = this.state.movie;
+    // console.log(this.state.movie)
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
+        <MovieCard card={this.state.movie} />
+        {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
             Director: <em>{director}</em>
@@ -61,7 +64,7 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div className="save-button">Save</div> */}
       </div>
     );
   }
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({ card }) => {
+  console.log(card)
+  return(
+    <div className="movie-card">
+      <h2>{card.title}</h2>
+      <div className="movie-director">
+        Director: <em>{card.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{card.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {card.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default MovieCard;
+
+
+
