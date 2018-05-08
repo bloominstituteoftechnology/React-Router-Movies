d3eafba8c7f46572254e67ea72704dc9501516ff
masontmorrow <masontmorrow@gmail.com>
2018-05-09 00:45:08 +0200

    Link to NavLink & Styling in index.css

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default class SavedList extends Component {
   
@@ -8,7 +8,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link key={movie.id} to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+          <NavLink key={movie.id} to={`/movies/${movie.id}`} className="saved-movie" activeClassName="saved-active">{movie.title}</NavLink>
           
         ))}
         <Link to="/">
