9b551288f2ec10def97e5cd8e036274cfb553e33
Bradiowave <divebombwalker@gmail.com>
2018-05-08 14:15:28 -0700

    turned saved movie list into liks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,17 +26,18 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  // Uncomment this code when you're ready for the stretch problems
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
@@ -46,7 +47,7 @@ export default class Movie extends Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={() => this.props.addToSavedList(this.state.movie)}>Save</div>
+        <div className="save-button" onClick={() => this.saveMovie()}>Save</div>
       </div>
     );
   }
