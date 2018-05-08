ee8f356360b6811d9e589d4e206c6c78de00ea67
emortalix <emortalix@welcometoforever.io>
2018-05-08 13:46:42 -0400

    Application of React Router Lambda School Training

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 export default class App extends Component {
+
+// const Index = props => (
+//   <h1>{props.something}</h1>
+// )
+
+// <Route path="/" render={() => <Index A/B/C={'here'} />} />
+
+
+
   constructor() {
     super();
     this.state = {
@@ -14,16 +24,21 @@ export default class App extends Component {
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+    if (!savedList.some(savedMovie => savedMovie.id === movie.id)) {
+      savedList.push(movie);
+      this.setState({ savedList });    }
   };
 
   render() {
     return (
       <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+          <SavedList list={this.state.savedList} />
+          <Route exact path="/" component={MovieList}></Route>
+          {/* <Route path="/movies/:id" component={Movie}></Route> */}
+          <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={this.addToSavedList}/>} />
+
       </div>
     );
   }
 }
+
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,10 @@
+
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
+
+
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -35,6 +40,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
+      <Link to={`/movies/${movie.id}/${movie.title.replace(/\s/g, '')}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -49,6 +55,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      </Link>
     </div>
   );
 }
