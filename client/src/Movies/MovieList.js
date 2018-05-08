4c6189dac6690eb0035e3b2dd103ba73fa7b4c39
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-08 07:03:23 +0700

    implemented movies routes to contain both id and movie title

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -36,7 +36,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <Link to={`/movies/${movie.id}`}>
+      <Link to={`/movies/${movie.id}/${movie.title.replace(/\s/g, '')}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
