e3db92e1a4f74d5c0bfb6ecc47676ccbcbbc12ac
Luis <luis@Luiss-MacBook-Air.local>
2018-06-25 11:37:40 -0700

    created a Route to MoiveList & Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import SavedList from './Movies/SavedList'
 import MovieList from './Movies/MovieList'
 import Movie from './Movies/Movie'
-
+import { BrowserRouter as Route } from 'react-router-dom'
 export default class App extends Component {
   constructor () {
     super()
@@ -23,6 +23,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movie/id' component={Movie} />
       </div>
     )
   }
