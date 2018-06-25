42a733fea21c9814e28fcb9668ca04f4927edc02
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-25 15:37:00 -0700

    Add semi-funtional links to movie cards

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,6 +12,7 @@ export default class Movie extends Component {
   componentDidMount() {
     // change this line to grab the id passed on the URL
     const id = 1;
+    console.log(this.state.movie);
     this.fetchMovie(id);
   }
 
