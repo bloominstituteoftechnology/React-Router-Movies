cfbcdd779e219e163fddcf991ea76c4e8809c2cf
Luis <luis@Luiss-MacBook-Air.local>
2018-06-25 14:15:02 -0700

    created routes & gave movie cards a link as well as the to have functionallity

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
       savedList: []
-    };
+    }
   }
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+  addToSavedList = (movie) => {
+    const savedList = this.state.savedList
+    savedList.push(movie)
+    this.setState({ savedList })
+  }
 
-  render() {
+  render () {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path='/' component={MovieList} />
+        <Route exact path='/movies/:id' component={Movie} />
       </div>
-    );
+    )
   }
 }
