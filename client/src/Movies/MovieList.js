02bd2d4959974d1586dc0d4a80da5ec75a0a978a
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-25 14:41:16 -0600

    Implement linked list of movies and home link

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -23,32 +24,17 @@ export default class MovieList extends Component {
   render() {
     return (
       <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
+        {this.state.movies.map(movie => (<MovieLink key={movie.id} movie={movie} />))}
       </div>
     );
   }
 }
 
-function MovieDetails({ movie }) {
+function MovieLink({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+            <div className="movie-card">
+          <h2><Link to={`/${movie.id}`} key={movie.id}>{movie.title}</Link></h2>
         </div>
-      ))}
-    </div>
-  );
+   );
 }
