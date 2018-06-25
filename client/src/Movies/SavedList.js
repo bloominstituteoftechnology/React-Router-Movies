73a780eabeaec54c44ac6b1a8a112fe1735c770a
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-25 16:15:28 -0600

    Add ugly navlinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/${movie.id}`}><span className="saved-movie">{movie.title}</span></Link>
+          <NavLink to={`/${movie.id}`}><span className="saved-movie" key={movie.id}>{movie.title}</span></NavLink>
         ))}
         <Link to='/' className="home-button">Home</Link>
       </div>
