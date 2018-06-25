4cbced79e02cb169049423f60b2b6c193dee0c43
Erin Meredith <ekmeredith@att.net>
2018-06-25 18:21:12 -0400

    corrected path for movie list

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route path='/movie-list' component={MovieList} />
+        <Route path='/' component={MovieList} />
         <Route path='/movies/:id' component={Movie} />
       </div>
     );
