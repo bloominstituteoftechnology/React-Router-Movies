71e2a02ff18d2cccd8ab39ab97118e78da504feb
Vu Cao <cdhvu@yahoo.com>
2018-06-25 15:42:18 -0700

    Completed stretch goal 2
    _Save button is now add movie to savedList if movie is not already in there.

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,10 @@ export default class App extends Component {
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+    if (!savedList.includes(movie)) {
+      savedList.push(movie);
+      this.setState({ savedList });
+    }
   };
 
   render() {
@@ -24,7 +26,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList}></Route>
-        <Route path="/movies/:id" component={Movie}></Route>
+        <Route path="/movies/:id" render={props => <Movie {...props} addToSavedList={this.addToSavedList}/>}></Route>
       </div>
     );
   }
