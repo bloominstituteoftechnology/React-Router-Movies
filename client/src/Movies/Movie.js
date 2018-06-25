a7bef8b543e0c3d8a1eaa893fb38619b0289b3fc
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-25 13:19:48 -0700

    adds back save button:)

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -42,9 +42,11 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
+    // const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
+        <div className="save-button">Save</div>
       </div>
     );
   }
