c5a4d4aa5aef643869a2a8f3cc170c9b5642656a
Tyson Gushiken <tysonism@gmail.com>
2018-06-25 17:37:00 -0700

    Convert SavedList to functional component and reset css

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+const SavedList = props => {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <NavLink
+        activeClassName="is-selected" to={`/movies/${movie.id}`}>
+          <span key={`save-${Math.random()}`} className="saved-movie">{movie.title}</span>
+        </NavLink>
+      ))}
+      <Link to="/">
+        <div className="home-button">Home</div>
+      </Link>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <NavLink
-          activeClassName="is-selected" to={`/movies/${movie.id}`}>
-            <span key={`save-${Math.random()}`} className="saved-movie">{movie.title}</span>
-          </NavLink>
-        ))}
-        <Link to="/">
-          <div className="home-button">Home</div>
-        </Link>
-      </div>
-    );
-  }
-}
+export default SavedList;
