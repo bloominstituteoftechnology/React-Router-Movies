9f263262cf205ff30ee84feb1a5f5a3c4ecab117
Erin Meredith <ekmeredith@att.net>
2018-06-26 12:30:50 -0400

    stretch started, but last item of stars.map is returning undefined

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
 import {Link} from 'react-router-dom';
 
+
+
 const MovieCard = props => {
-  return <Link to='/movie/:id'>{props.movie}</Link>
-};
+
+  return (
+    <div className="movie-card"><Link to={`/movies/${props.id}`} className="movie-card">
+      <h2>{props.title}</h2>
+      {console.log(props.title)}
+      <div className="movie-director">
+        Director: <em>{props.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+{console.log(props.stars)}
+      {props.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+      </Link>
+    </div>
+
+  );
+}
 
 export default MovieCard;
