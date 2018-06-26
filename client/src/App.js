7ff4deed0769c95d494212b77b18257b5b771c86
Randy Calderon <randycweb@gmail.com>
2018-06-26 13:09:02 -0700

    Wrapped app with Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -20,10 +21,12 @@ export default class App extends Component {
 
   render() {
     return (
+      <Router>
       <div>
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
       </div>
+      </Router>
     );
   }
 }
