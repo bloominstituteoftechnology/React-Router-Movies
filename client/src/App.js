94748b0abd12f6bb8837fd8351be32cca4d603a6
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-26 16:29:53 -0400

    First commit, day 2
    A whole mess of changes
    Some styling on the cards in MovieList
    Added save functionality
    Refactored Movie and MovieCard
    Completed stretch problems

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import styled from 'styled-components'
 
 import { Route } from "react-router-dom"; 
+
+const ClickInfo = styled.h3`
+  text-align: center;
+  margin-top: 2.5%;
+`
 
 export default class App extends Component {
   constructor() {
@@ -24,8 +30,11 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
+        <ClickInfo>Click on the movie cards below for more details...</ClickInfo>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route path="/movies/:id" render={ (props) => {
+          return(<Movie {...props} 
+            addToSavedList={this.addToSavedList}/>)}} />
       </div>
     );
   }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom'
+
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+        color: black;
+    }
+`;
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -10,9 +23,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`} key={movie.id} activeClassName="saved-active">
+            <span className="saved-movie">{movie.title}</span>
+          </NavLink>
         ))}
-        <div className="home-button">Home</div>
+        <div className="home-button"><StyledLink to="/">Home</StyledLink></div>
       </div>
     );
   }
