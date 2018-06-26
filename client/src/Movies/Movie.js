9b708a52120398d7225ed1fa1b2efeacc2942f5f
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-26 16:56:01 -0500

    Ahmed Belhadj - React Router Movies
    Refactored code to use MovieCard component.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -43,27 +43,9 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    // const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <MovieCard movie={this.state.movie}/>
-
-        {/* <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div> */}
+        <MovieCard movie={this.state.movie} />
         <div className="save-button">Save</div>
       </div>
     );
