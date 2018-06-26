0d4069ca35c9813ac3837550f7f2177b12d4faff
-tristan- <tristan.linehan@gmail.com>
2018-06-26 14:42:29 -0700

    refactored dry code from movie.js & movieList.js, by creating component<MovieCard>, stretch-1

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom' ;
 import axios from 'axios';
+import MovieCard from './MovieCard' ;
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -35,23 +36,11 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movie/${movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-    </Link>
+    <div>
+      <Link to={`/movie/${movie.id}`}>        
+        <MovieCard movie={movie} />
+      </Link>
+    </div>
   );
 }
+
