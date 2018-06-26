c2780896224480b5a4fc726ea65190d79ed714f6
Simon <Simon_Freelance@hotmail.com>
2018-06-26 17:46:12 -0400

    Implemented NavLink component in SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -13,9 +13,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map((movie, i) => (
-          <span className="saved-movie" key = {i} onClick={() => {
-            this.goToLink(`/movies/${movie.id}`);
-            }}>{movie.title}</span>
+          <NavLink to = {`/movies/${movie.id}`} activeStyle={{fontWeight: 'bold', color: 'red'}}>
+          <span className="saved-movie" key = {i}>{movie.title}</span>
+            </NavLink>
         ))}
         <Link to = '/' className="home-button">Home</Link>
       </div>
