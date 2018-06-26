8ba34d2be94712750376756a7c893be66eb048bc
Simon <Simon_Freelance@hotmail.com>
2018-06-26 17:24:42 -0400

    Each saved movie has a link to get the movie details

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,13 +4,18 @@ export default class SavedList extends Component {
   constructor(props) {
     super(props);
   }
-
+  goToLink = (url) => {
+    this.props.history.push(url);
+    
+  }
   render() {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map((movie, i) => (
-          <span className="saved-movie" key = {i}>{movie.title}</span>
+          <span className="saved-movie" key = {i} onClick={() => {
+            this.goToLink(`/movies/${movie.id}`);
+            }}>{movie.title}</span>
         ))}
         <Link to = '/' className="home-button">Home</Link>
       </div>
