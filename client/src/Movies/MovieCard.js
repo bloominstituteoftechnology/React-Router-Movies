389059d8964e07c4e590a0195c974bffc2c1cf1a
TangledTessellations <mcevoya@oregonstate.edu>
2018-07-05 15:38:08 -0700

    Add card styling and NavLinks

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    text-decoration: none;
+    background: linear-gradient(to top, orange, black);
+    border 1px solid white;
+`;
+
+const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+`;
 
 const MovieCard = props => {
   const movie = props.movie;
   const { title, director, metascore, stars } = movie;
   return (
-      <Link to={`/movies/${movie.id}`} >
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-  
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-      </Link>
+        <StyledNavLink to={`/movies/${movie.id}`}
+                  activeClassName="selected"
+                  activeStyle={{
+                    color: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }} >
 
+          <StyledDiv className="movie-card">
+            <h2>{title}</h2>
+            <div className="movie-director">
+              Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+              Metascore: <strong>{metascore}</strong>
+            </div>
+            <h3>Actors</h3>
     
+            {stars.map(star => (
+              <div key={star} className="movie-star">
+                {star}
+              </div>
+            ))}
+          </StyledDiv>
+        </StyledNavLink>
     );
 };
 
