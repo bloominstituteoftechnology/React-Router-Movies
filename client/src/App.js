e6911937ea7e85758447e2c634fc5c161520bcfb
Ashwin <aswinjeganath@gmail.com>
2018-08-07 02:03:39 +0530

    Add save movie to list functionality

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
   }
 
   addToSavedList = movie => {
-    const savedList = this.state.savedList;
+    const savedList = this.state.savedList.slice();
     savedList.push(movie);
     this.setState({ savedList });
   };
@@ -24,7 +24,13 @@ export default class App extends Component {
         <div>
           <SavedList list={this.state.savedList} />
           <Route exact path="/" component={MovieList} />
-          <Route exact path="/movies/:id" component={Movie} />
+          <Route
+            exact
+            path="/movies/:id"
+            render={props => (
+              <Movie {...props} addToSavedList={this.addToSavedList} />
+            )}
+          />
         </div>
       </Router>
     );
