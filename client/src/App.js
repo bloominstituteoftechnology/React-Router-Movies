1f1efb32d3e746fc86e4a60b69f0bd66a3d0d717
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-08-06 16:19:53 -0600

    Got it working. Need to go over it again.

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route path="/" Component={MovieList} />
-        <Route path="/movies/:id" Component={Movie} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" 
+        render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />}
+        />
       </div>
     );
   }
