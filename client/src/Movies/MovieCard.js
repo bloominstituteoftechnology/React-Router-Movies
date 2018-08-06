63eb4d0d3570017945a899ddd73754398a7f2fcf
johnoro <jjcummins102@gmail.com>
2018-08-06 17:45:43 -0400

    Added styling.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -12,11 +12,10 @@ const MovieCard = props => {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
       {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
+        <ul key={star} className="movie-star">
+          <li>{star}</li>
+        </ul>
       ))}
     </div>
   );
