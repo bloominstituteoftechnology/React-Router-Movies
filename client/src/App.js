f8a6af0017d0dcee0820f2cda0438e588eb2e8c5
dasgma <dasgrigoma@gmail.com>
2018-08-06 13:55:45 -0700

    Made the movie be able to save.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -24,7 +24,13 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movie/:id" component={Movie} />
+        <Route path="/movie/:id" 
+          render = {(props) => 
+            <Movie  {...props} addToSavedList={this.addToSavedList} />
+          }
+        />
+
+
         </div>
       </div>
     );
