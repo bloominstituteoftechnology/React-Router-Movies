28c5a67c444a101c4d98a8771b789b54400590fb
codejoncode <jonathanjholloway@gmail.com>
2018-08-06 15:41:13 -0500

    refactored the code so that it is dry. MovieCard is now on the MovieDetails component and the Movie component and after a test everything works like it did before

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -39,22 +39,7 @@ function MovieDetails({ movie }) {
   return (
     <Link to = {`/movies/${parseInt(id,10)}`}>
       <MovieCard movie ={movie}/>
-      {/* <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div> */}
+      
     </Link>
   );
 }
