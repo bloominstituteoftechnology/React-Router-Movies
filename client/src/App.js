48b8acfc5b2d8158fd0a155cbd0897fc431ae38a
szincone <sawyerzincone@gmail.com>
2018-08-06 16:30:55 -0600

    added if statement to prevent duplicated in saved movies

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,12 @@ export default class App extends Component {
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+    if (!this.state.savedList.includes(movie)) {
+      savedList.push(movie);
+      this.setState({ savedList });
+    } else {
+      // do nothing
+    }
   };
 
   render() {
