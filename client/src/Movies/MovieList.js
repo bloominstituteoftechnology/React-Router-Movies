a69f71b6fa79a413bfbe5b9f9521cb7f49b43680
Josue Peralta <jperal77@gmail.com>
2018-08-06 16:23:50 -0700

    Completed refactoring stretch goal

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -36,21 +37,8 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars , id} = movie;
   console.log(id);
   return (
-    <Link to={`/movies/${id}`} ><div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div></Link>
+    <Link to={`/movies/${id}`} >
+      <MovieCard movie={movie} />
+    </Link>
   );
 }
