3343692706c399bd63b529295dd1094d98e05df9
rodacoram <rodacoram8@gmail.com>
2018-08-06 13:42:50 -0600

    React-Router--Rodrigo-Acosta-Ramos
    I think part one is done. So much is already written for us.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Link} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
@@ -20,9 +21,10 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path= '/movies' component={MovieList} />
+        <Route exact path= '/movies/:id' component={Movie} />
       </div>
     );
   }
