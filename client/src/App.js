4949596a54eb5269add3f183fce15e6689bdec8e
Carey <careycbaldwin@gmail.com>
2018-08-06 15:52:25 -0400

    completed part 1 - two routes for MovieList and Movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//Import Router, Route, Link
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 import SavedList from './Movies/SavedList';
@@ -26,7 +26,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path= "/" component = {MovieList} />
+        <Route path = "/movies/:id" component = {Movie} />
       </div>
     );
   }
