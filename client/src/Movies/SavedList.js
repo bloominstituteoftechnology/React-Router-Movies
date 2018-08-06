32699c7f31d6a6a07c4c6bf76108a15458b00df5
johnoro <jjcummins102@gmail.com>
2018-08-06 18:39:38 -0400

    Got rid of console warnings.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,11 +7,12 @@ const SavedList = props => {
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <NavLink
+          key={movie.src}
           to={`/movies/${movie.id}`}
           activeClassName="saved-active"
           className="saved-movie"
         >
-          <img className="saved-img" src={movie.src} />
+          <img className="saved-img" src={movie.src} alt="" />
         </NavLink>
       ))}
       <Link to="/" className="home-button">
