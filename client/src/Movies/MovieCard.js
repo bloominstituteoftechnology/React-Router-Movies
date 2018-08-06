9f91fc4d5cef9082f4fefc22ad2a176929a6a7b0
johnoro <jjcummins102@gmail.com>
2018-08-06 18:30:12 -0400

    Added images for each movie.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,22 +1,31 @@
 import React from "react";
 
 const MovieCard = props => {
-  const { title, director, metascore, stars } = props.movie;
+  const { title, director, metascore, stars, src } = props.movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-      {stars.map(star => (
-        <ul key={star} className="movie-star">
-          <li>{star}</li>
+    <div className="movie-card-wrapper">
+      <img
+        className="movie-img"
+        src={src}
+        alt=""
+      />
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+        <ul className="movie-stars">
+          {stars.map(star => (
+            <li key={star} className="movie-star">
+              {star}
+            </li>
+          ))}
         </ul>
-      ))}
+      </div>
     </div>
   );
 };
