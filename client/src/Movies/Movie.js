374a0438e3c7db4ecded9a83a10f56d81f453ddf
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-13 16:04:00 -0400

    updated componentDidMount on movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -10,9 +11,7 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = 1;
-    this.fetchMovie(id);
+    this.fetchMovie(this.props.match.params.id);
   }
 
   fetchMovie = id => {
