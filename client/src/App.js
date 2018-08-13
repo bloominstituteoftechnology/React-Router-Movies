5b9b7a4636baa440053a5c981fa697a9e5fb9552
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-13 16:42:49 -0400

    added home button functionality

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,9 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component = {MovieList}/>
-        <Route path="/movies/:id"/>
+        <Route path="/movies/:id"
+        render={(props) => <Movie {...props} />}
+        />
       </div>
     )
   }
