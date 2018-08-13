62f83199b5312e9a92ed70889fb3848e1ddda3c1
bri79319 <bri79319@gmail.com>
2018-08-13 16:28:34 -0400

    attempting to Link movie and movielist

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowswerRouter as Router } from "react-router-dom";
-
+import { BrowserRouter as Router } from "react-dom";
 import './index.css';
 import App from './App';
 
@@ -11,4 +10,3 @@ ReactDOM.render(
     </Router>, 
     document.getElementById('root')
 );
-
