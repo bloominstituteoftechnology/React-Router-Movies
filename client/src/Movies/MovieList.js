589f840e41e516bc0c8a0888dded065e5d51377a
bri79319 <bri79319@gmail.com>
2018-08-13 22:31:34 -0400

    changes to code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
-import MoiveCard from "./MovieCard";
 import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
