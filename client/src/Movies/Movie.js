414acf6ff69b31961f4a2cc504dcfeb4ce1dfee6
vanceleon <vanceleon44@gmail.com>
2018-08-13 20:09:34 -0400

    text-decoration set to none, links all work

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,9 +11,10 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
-    this.fetchMovie(id);
-  }
+      // const {id} = this.props.match.params.id
+      this.fetchMovie(this.props.match.params.id)
+ 
+    }
 
   fetchMovie = id => {
     axios
