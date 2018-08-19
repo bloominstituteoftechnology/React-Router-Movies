8549df549a7937bf4fed1781b978d087fefcd0b9
JustinSHong <jhong2009@gmail.com>
2018-08-19 13:35:24 -0700

    set MovieCard title to be a link with no text decoration

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -8,24 +8,24 @@ import { Link } from "react-router-dom";
 const MovieCard = props => {
   const { title, director, metascore, stars } = props.movie;
   return (
-    <Link to={`/movies/${props.movie.id}`}>
-      <div className="movie-card">
+    <div className="movie-card">
+      <Link className="movie-card-link" to={`/movies/${props.movie.id}`}>
         <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+      </Link>
+      <div className="movie-director">
+        Director: <em>{director}</em>
       </div>
-    </Link>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
   );
 };
 
