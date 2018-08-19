298f9d9601898ae3bb2fee5d0bed107bb4cb11db
JustinSHong <jhong2009@gmail.com>
2018-08-19 13:48:42 -0700

    changed saved movie title links to be black, bold, and have no text-decoration

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -13,17 +13,19 @@ export default class SavedList extends Component {
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
           <NavLink
-            to={`/movies/${movie.id}`}
             activeStyle={{
-              fontWeight: "bold",
-              color: "red"
-            }}>
+              textDecoration: "none",
+              color: "black",
+              fontWeight: "bold"
+            }}
+            to={`/movies/${movie.id}`}>
             <span className="saved-movie">{movie.title}</span>
           </NavLink>
         ))}
         <Link to="/" className="home-button">
           Home
         </Link>
+        <button className="clear-button">Clear</button>
       </div>
     );
   }
