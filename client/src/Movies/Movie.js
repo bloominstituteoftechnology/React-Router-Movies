fbca3a07dadecb1d987bb490422fc4abf8eb4464
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-09-08 16:15:49 -0700

    finished mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,8 +11,8 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = 1;
-    this.fetchMovie(id);
+    const movieId = this.props.match.params.id;
+    this.fetchMovie(movieId);
   }
 
   fetchMovie = id => {
