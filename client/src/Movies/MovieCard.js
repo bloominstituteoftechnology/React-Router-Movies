0f895c3cf2daece0d56cfab2dfe41b1b422f76a1
Your Name <tjkisner@gmail.com>
2018-09-10 18:32:28 -0400

    MVP plus some stretches

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,8 @@
-import React from 'react';
+// import React from 'react';
+// import {Link} from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
-};
+// const MovieCard = props => {
+//   return <div><Link to='/movies/:props.id' ></Link> </div>;
+// };
 
-export default MovieCard;
+// export default MovieCard;
