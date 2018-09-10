c39bdc856d54f474b2b48b2bba78169369e08269
m-j-hacker <mjhacker@gmail.com>
2018-09-10 16:43:14 -0500

    Added Save button functionality to project

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={Movie} />
+        <Route exact path="/movies/:id" component={(props)=> <Movie {...props} addToSavedList={this.addToSavedList}/>}/>
       </div>
     );
   }
