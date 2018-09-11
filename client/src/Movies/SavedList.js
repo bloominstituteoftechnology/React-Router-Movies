042ffd91d337cecb3e2c7ec7f82103bed818d34d
unknown <cornielleandres@gmail.com>
2018-09-10 20:49:16 -0400

    Complete stretch #4: Turn your Saved Movie Links into NavLinks.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+
+import './SavedList.css';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -7,13 +9,19 @@ export default class SavedList extends Component {
   }
 
   render() {
+    const activeStyle = {
+      border: '1px solid black',
+      borderRadius: '5px',
+      padding: '5px',
+    };
+
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link key = {movie.id} to = { `/movies/${movie.id}` }>
+          <NavLink key = {movie.id} to = { `/movies/${movie.id}` } activeStyle = { activeStyle }>
             <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         ))}
         <Link to = '/'>
           <div className="home-button">Home</div>
