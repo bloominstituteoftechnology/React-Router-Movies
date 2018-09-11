91d990c6ffa9e86c89db7ef2047fafe7bc658071
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-11 17:36:50 -0400

    Added save movie functionality need to work on save function only displaying the movie once upon multiple clicks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -27,16 +27,16 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+   componentWillReceiveProps(newProps){
+     if(this.props.match.params.id !== newProps.match.params.id){
+       this.fetchMovie(newProps.match.params.id);
+     }
+   }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+     const addToSavedList = this.props.addToSavedList;
+     addToSavedList(this.state.movie)
+   }
 
   render() {
     if (!this.state.movie) {
@@ -46,9 +46,9 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-       <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
-       <div className="save-button"> Save</div>
-       </div>
+      <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
+      <div className="save-button" onClick={this.saveMovie}>Save</div>
+    </div>
     );
   }
 }
