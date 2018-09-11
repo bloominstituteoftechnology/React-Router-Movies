43dde2cc132db9a931677f2455e661531d9eaf93
alejandrok93 <alejandrok93@gmail.com>
2018-09-11 16:11:27 -0500

    Added functionality to save movies and made MovieCard component to follow DRY coding practices

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import MovieCard from "./MovieCard";
 export default class Movie extends Component {
   constructor(props) {
     super(props);
@@ -41,27 +41,15 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
-    const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
+        <MovieCard movie={this.state.movie} />
+        <div
+          onClick={event => this.props.addToSavedList(this.state.movie)}
+          className="save-button"
+        >
+          Save
         </div>
-        <div className="save-button">Save</div>
       </div>
     );
   }
