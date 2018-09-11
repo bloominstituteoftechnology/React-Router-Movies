222463e6bee73f5af141ca817fbde255e620c0c3
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-11 15:42:04 -0400

    Huthman King - RR Movies wrapped app with router added routes that load movie and movielist

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+
+
 
 export default class App extends Component {
   constructor() {
@@ -22,7 +25,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route path='/' component={MovieList}></Route>
+        <Route path='/:id' component={Movie}></Route>
       </div>
     );
   }
