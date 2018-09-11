954646c5e7a74f15828df5fc891bbcf4643aecbe
Clint Kunz <clintkunz@gmail.com>
2018-09-11 15:32:14 -0600

    made savedList a list of Links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -10,7 +11,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <Link to={`/movie/${movie.id}`} className="saved-movie">{movie.title}</Link>
         ))}
         <div>
           <div onClick={() => this.props.history.push('/')} className="home-button">Home</div>
