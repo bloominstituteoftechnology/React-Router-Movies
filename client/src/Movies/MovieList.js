def9be079d150dee95a36e8429d740971f5f8549
mich-shrak <micah.shrak@gmail.com>
2018-09-17 16:31:32 -0700

    Fixed movie-card

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,17 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const movieCard = {
-	backgroundColor: '#fff',
-	border: '0',
-	boxShadow: '0 -1px 0 #e0e0e0',
-	WebKitBoxShadow: '0 0 2px rgba(0, 0, 0, 0.12)',
-	MozBoxShadow: '0 2px 4px rgba(0, 0, 0, 0.24)',
-	padding: '1rem',
-	cursor: 'pointer',
-	position: 'relative',
-	margin: '1rem auto',
-};
 export default class MovieList extends Component {
 	constructor(props) {
 		super(props);
@@ -46,8 +35,10 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
 	const { title, director, metascore, stars } = movie;
 	return (
-		<Link to={`/movies/${movie.id}`} style={movieCard}>
-			<h2>{title}</h2>
+		<div className="movie-card">
+			<Link to={`/movies/${movie.id}`}>
+				<h2>{title}</h2>
+			</Link>
 			<div className="movie-director">
 				Director: <em>{director}</em>
 			</div>
@@ -61,6 +52,6 @@ function MovieDetails({ movie }) {
 					{star}
 				</div>
 			))}
-		</Link>
+		</div>
 	);
 }
