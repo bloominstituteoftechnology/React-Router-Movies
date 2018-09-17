b2a29c5ad94a9bdbedd19102696df02e1fced3b0
verydecent <wnjhwng@gmail.com>
2018-09-17 15:22:31 -0700

    Rendering Movie List, Added Link to each Movie

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -24,12 +25,16 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <Link to={`/movies/${movie.id}`}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link> 
+
         ))}
       </div>
     );
   }
 }
+
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
