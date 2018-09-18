7159e3b3b553f729c131068c045418da0ade61d6
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-18 11:45:15 -0400

    Refactored MovieDetails into MovieCard and wrapped in Link jsx element

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+
 
 const MovieCard = props => {
-  return;
+  return (
+    <React.Fragment>
+      <h2>{props.movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{props.movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{props.movie.metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {props.stars.map(star => (
+        <div key={star} className="movie-star">
+          {props.movie.star}
+        </div>
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default MovieCard;
