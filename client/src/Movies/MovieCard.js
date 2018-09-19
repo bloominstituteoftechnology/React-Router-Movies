24bb4408c70304bcd9d6d914b4af7c8baba5250a
verydecent <wnjhwng@gmail.com>
2018-09-18 19:17:01 -0700

    Created MovieCard and replaced DRY code with component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
-};
+function MovieCard({ movie }) {
+  const { id, title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <Link to={`/movies/${id}`}>
+        <h2>{title}</h2>
+      </Link>
+      <div>
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default MovieCard;
+
