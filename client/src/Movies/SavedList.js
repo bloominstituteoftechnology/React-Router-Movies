0409e055873d8f8d02273fa7d3ca6676a423259f
Stublink <chrisrey95@gmail.com>
2018-10-15 16:53:25 -0400

    Turned SavedList into Links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,7 +11,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <Link to={`/movie/${movie.id}`}  style={{textDecoration: 'none', color: 'black'}}>
+            <span className="saved-movie">{movie.title}</span>
+          </Link>
         ))}
         <Link to="/"style={{textDecoration: 'none', color: 'black'}} >
           <div className="home-button">Home</div>
