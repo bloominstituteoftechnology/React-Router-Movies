a9093942fda8526cf97893232ac3deec6ef29b92
ssmodish <smodish@hotmail.com>
2018-10-15 17:35:56 -0400

    MovieCard Stretch completed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import MovieCard from './MovieCard';
 
 import axios from 'axios';
 
@@ -48,22 +49,7 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
+        <MovieCard movie={ this.state.movie } />
         <div className="save-button">Save</div>
       </div>
     );
