d2a32ba682a334906802acfe943b3ea2a9fcb129
Brian G <brian.gregorius@gmail.com>
2018-10-15 15:49:01 -0400

    added link to and single movie page rendering correctly

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
         <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={Movie} />
 
 
       </div>
