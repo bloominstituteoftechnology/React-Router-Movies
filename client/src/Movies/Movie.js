08262de600a0d75cca6463f74259403de5a9350d
Axolotl <samuelmachat@gmail.com>
2018-10-15 16:44:20 -0400

    refactored code to be more dry

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
+import MovieCard from './MovieCard'
 
 export default class Movie extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       movie: null
-    };
+    }
   }
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    const id = this.props.match.params.id;
-    this.fetchMovie(id);
+    const id = this.props.match.params.id
+    this.fetchMovie(id)
   }
 
   fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
-        this.setState(() => ({ movie: response.data }));
+        this.setState(() => ({ movie: response.data }))
       })
       .catch(error => {
-        console.error(error);
-      });
-  };
+        console.error(error)
+      })
+  }
   // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
   //   if(this.props.match.params.id !== newProps.match.params.id){
@@ -39,30 +40,16 @@ export default class Movie extends Component {
 
   render() {
     if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
+      return <div>Loading movie information...</div>
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    const { title, director, metascore, stars } = this.state.movie
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
+        <MovieCard {...this.state.movie} />
 
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
         <div className="save-button">Save</div>
       </div>
-    );
+    )
   }
 }
