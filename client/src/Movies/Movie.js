e15337e9567e75576c37bc40109d9e1b3dd25573
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-10-15 16:19:22 -0600

    react router movies part1

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,7 +10,7 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    const id = this.props.match.params.id;
+    const { id } = this.props.match.params.id;
     // change this line to grab the id passed on the URL
     const id = 1;
     this.fetchMovie(id);
