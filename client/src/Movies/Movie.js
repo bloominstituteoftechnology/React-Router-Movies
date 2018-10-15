c6984f9e5e7ba41e73d1d70cfc9e21a4f646f139
JacobABrennan <jacobabrennan@gmail.com>
2018-10-15 17:55:49 -0400

    Save movies on click

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,25 +26,26 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
+    console.log(this.props)
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button">Save</div>
+        <div onClick={this.saveMovie} className="save-button">Save</div>
       </div>
     );
   }
