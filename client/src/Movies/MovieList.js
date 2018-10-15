7c2d0a29b543f1f0d9825f156ab9b6cacc1dc58a
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-15 13:00:16 -0700

    finished day 1 tasks

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,7 +35,8 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link><div className="movie-card">
+    <div className="movie-card">
+    <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -50,6 +51,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div></Link>
+      </Link>
+    </div>
   );
 }
