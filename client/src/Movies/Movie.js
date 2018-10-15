da2f847571f6c3303eb28890a4b341d076787d0e
Adam <ajmboosh@gmail.com>
2018-10-15 16:28:22 -0500

    Stretch for day2 complete: code in MovieList and Movie has been made DRY by returning a new component MovieCard twice instead of writing out its code twice

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 export default class Movie extends Component {
   constructor(props) {
@@ -20,7 +21,6 @@ export default class Movie extends Component {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
-        console.log(response.data);
         this.setState(() => ({ movie: response.data }));
       })
       .catch(error => {
@@ -43,28 +43,7 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        <div className="save-button">Save</div>
-      </div>
-    );
+    //replaced old code which wrote out everything now in MovieCard.js a second time
+    return <MovieCard movie={this.state.movie} />;
   }
 }
