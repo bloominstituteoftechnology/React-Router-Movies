28883b4a0be61add91228042144ea60c7783fcb1
BryanGf <briangf@uw.edu>
2018-10-15 15:07:43 -0700

    Saved Movies

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
 import MovieCard from './MovieCard';
+import SavedList from './SavedList';
 
 export default class Movie extends Component {
   constructor(props) {
@@ -27,28 +28,29 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
     const movie = this.state.movie;
     return (
       <div className="save-wrapper">
           <MovieCard movie={movie}/>
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={(event) => {
+          this.props.addToSavedList(movie)
+        }}>Save</div>
       </div>
       
     );
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -7,16 +7,18 @@ export default class SavedList extends Component {
   }
 
   render() {
+    console.log(this.props.list)
     return (
-      <Link to='/'>
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
+        <Link to='/'>
         <div className="home-button">Home</div>
+        </Link>
       </div>
-      </Link>
+      
     );
   }
 }
