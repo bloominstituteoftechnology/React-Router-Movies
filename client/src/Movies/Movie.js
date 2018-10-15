c5e5e1d5ac0d4e7e3dd8441f3b84358ddfb2e772
Daniel Ferrer <danfrrr13@gmail.com>
2018-10-15 15:39:11 -0400

    fixed small bug. Some stretch completed.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -18,7 +18,7 @@ export default class Movie extends Component {
 
   fetchMovie = id => {
     axios
-      .get(`http://localhost:5000/api/movies/0`)
+      .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
         this.setState(() => ({ movie: response.data }));
       })
@@ -27,19 +27,18 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
-    console.log(this.state.movie);
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
@@ -63,7 +62,8 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div onClick={this.saveMovie} 
+        className="save-button">Save</div>
       </div>
     );
   }
