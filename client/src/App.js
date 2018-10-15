db45057c06d38b976d6cbdc5d923b9626a02a9e9
Ryan Boris <neodynia@icloud.com>
2018-10-15 18:10:49 -0400

    Builds out app structure and rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import { Route } from 'react-router-dom'
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+    constructor() {
+        super()
+        this.state = {
+            savedList: []
+        }
+    }
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+    addToSavedList = (movie) => {
+        let savedList = this.state.savedList
+        if (!savedList.includes(movie)) {
+            savedList.push(movie)
+            this.setState({ savedList })
+    }
+}
 
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
-    );
-  }
+    clearList = () => {
+        this.setState({
+            savedList: []
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SavedList list={this.state.savedList} clearList={this.clearList} />
+                <Route exact path="/" component={MovieList} />
+                <Route
+                    path="/movies/:id"
+                    render={(props) => (
+                        <Movie
+                            {...props}
+                            addToSavedList={this.addToSavedList}
+                            savedList={this.state.savedList}
+                            clearList={this.clearList}
+                        />
+                    )}
+                />
+            </div>
+        )
+    }
 }
