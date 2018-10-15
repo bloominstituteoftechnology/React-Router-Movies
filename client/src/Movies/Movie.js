d980672b4a6b7e9f494574370c523474b3fedd01
Axolotl <samuelmachat@gmail.com>
2018-10-15 17:15:25 -0400

    convert saved movie list into links

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,31 +26,30 @@ export default class Movie extends Component {
         console.error(error)
       })
   }
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>
     }
 
-    const { title, director, metascore, stars } = this.state.movie
-    const { addToSavedList } = this.props
+    const { saveMovie } = this
 
     return (
       <div className="save-wrapper">
         <MovieCard {...this.state.movie} />
 
-        <div className="save-button" onClick={() => addToSavedList(title)}>
+        <div className="save-button" onClick={saveMovie}>
           Save
         </div>
       </div>
