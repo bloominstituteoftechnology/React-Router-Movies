8ecdee69f830e371147be6e389b7cc004fe5b119
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-15 17:15:42 -0600

    saved list is now clickable

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,8 +11,10 @@ export default class SavedList extends Component {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+        {this.props.list.map(movie => ( 
+          <Link to = {`/movies/${movie.id}`} key={movie.id}>
+            <span className="saved-movie" >{movie.title}</span>
+          </Link>
         ))}
         <Link to = "/" className="home-button">Home</Link>
       </div>
