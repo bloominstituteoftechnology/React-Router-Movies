a0b095da5f67a9711a0ea9380e24aa44119ebdec
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-16 03:19:42 -0400

    restructured some code, when you click on a movie you can see the details for the selected movie

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const MovieCard = props => {
+const MovieCard = ({title, director, metascore, stars}) => {
   return(
     <div className="movie-card">
-    {props.movies}
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
     </div>
   );
 };
