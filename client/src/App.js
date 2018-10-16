c6ae66bb8947083f29ab1eee6a7c15f95e595d23
Carlos Lantigua <clantigua954@gmail.com>
2018-10-16 17:19:30 -0400

    changed links in savedList to navlinks

--- client/src/App.js
+++ client/src/App.js
@@ -19,19 +19,21 @@ export default class App extends Component {
 		this.setState({ savedList });
 	};
 
-	removeFromList = (movie) => {
-		const savedList = this.state.savedList;
-		const id = movie.id;
+	// removeFromList = (movie) => {
+	// 	const savedList = this.state.savedList;
+	// 	const id = movie.id;
 
-		savedList.splice(id);
-		this.setState({ savedList });
-	};
+	// 	savedList.splice(id);
+	// 	this.setState({ savedList });
+	// };
 
 	render() {
 		return (
 			<div>
-				<SavedList list={this.state.savedList} removeFromList={this.removeFromList} />
-				{/* <Route path="/movies/:id" render={(props) => <Movie {...props} movies={MovieList} />} /> */}
+				<SavedList
+					list={this.state.savedList}
+					// removeFromList={this.removeFromList}
+				/>
 				<Route exact path="/" render={(props) => <MovieList {...props} />} />
 				<Route
 					path="/movies/:id"
