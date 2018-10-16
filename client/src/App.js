4cbe256ce3a9ad65b793ce988b1efa51bc4f26b9
Carlos Lantigua <clantigua954@gmail.com>
2018-10-15 22:47:21 -0400

    completed home link button

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,8 @@ export default class App extends Component {
 			<div>
 				<SavedList list={this.state.savedList} />
 				{/* <Route path="/movies/:id" render={(props) => <Movie {...props} movies={MovieList} />} /> */}
-				<Route exact path="/" component={MovieList} />
-				<Route path="/movies/:id" component={Movie} />
+				<Route exact path="/" render={(props) => <MovieList {...props} />} />
+				<Route path="/movies/:id" render={(props) => <Movie {...props} />} />
 			</div>
 		);
 	}
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 export default class SavedList extends Component {
 	constructor(props) {
 		super(props);
 	}
 
-	goHome() {
-		return <Route exact path="/" component={SavedList} />;
-	}
+	goHome = () => {
+		this.props.history.push(`/movies/`);
+	};
 
 	render() {
 		return (
 			<div className="saved-list">
 				<h3>Saved Movies:</h3>
 				{this.props.list.map((movie) => <span className="saved-movie">{movie.title}</span>)}
-				<div className="home-button">Home</div>
+				<Link to="/" className="home-button">
+					Home
+				</Link>
 			</div>
 		);
 	}
