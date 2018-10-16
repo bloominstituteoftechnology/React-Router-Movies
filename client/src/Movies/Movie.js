d325bbf16d5c78f3d76c62cbde1b5f9de654c31f
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-16 15:55:54 -0500

    stretch and styling -- still working on stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,7 +10,6 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
     const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
@@ -25,28 +24,29 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  componentWillReceiveProps(newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
+
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    const { img, title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
         <div className="movie-card">
           <h2>{title}</h2>
+          <img src={img} alt={title} />
           <div className="movie-director">
             Director: <em>{director}</em>
           </div>
@@ -61,7 +61,9 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={this.saveMovie}>
+          Save
+        </div>
       </div>
     );
   }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -38,21 +38,8 @@ function MovieDetails({ movie }) {
   const { img, title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <h1>{title}</h1>
       <img src={img} />
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
     </div>
   );
 }
