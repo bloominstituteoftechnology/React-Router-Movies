a9c71c4fabf9e237ec3a420bda72751b615e37df
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-16 02:08:23 -0400

    imported Route and fixed a bug

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+import { Route } from 'react-router-dom';
+
 export default class App extends Component {
   constructor() {
     super();
@@ -23,7 +25,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" render={props => <MovieList {...props} />} />
-        <Route path="/movies/:id" render={props => <MovieList {...props} />} />
+        <Route path="/movies/:id" render={props => <Movie {...props} />} />
       </div>
     );
   }
