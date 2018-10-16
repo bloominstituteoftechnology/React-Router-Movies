a29e2f53709bc88206c666a2619a8461b48e93c6
foritworld123@yahoo.com <foritworld123@yahoo.com>
2018-10-16 16:54:23 -0400

    MovieCard component added

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard.js';
 
 export default class Movie extends Component {
   
@@ -47,25 +48,10 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-
-          
+        <MovieCard/>
+        <div className="save-button" >
+            Save
         </div>
-        <div className="save-button" >Save</div>
       </div>
     );
   }
