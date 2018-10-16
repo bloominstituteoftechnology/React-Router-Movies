8735f5684a96e4eb8c09293177cccf3c4119cfbd
Troy Amlee <troyamlee19@gmail.com>
2018-10-15 21:05:12 -0500

    [stretch] Got first stretch figured out

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -44,30 +44,9 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-    const { title, director, metascore, stars } = this.state.movie;
-    
     return (
-      <div className="save-wrapper">
-            <div className="movie-card">
-      <Link to={`/movies/${ this.state.movie.id }`}>
-        <h2>{title}</h2>
-      </Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-        <div className="save-button">Save</div>
-      </div>
-    );
+     <MovieCard movie={this.state.movie} /> 
+    )
+    
   }
 }
