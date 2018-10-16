839dd55758a8c702450756e4fc8dc054e69cb08f
Riley-Brown <riley@riley.gg>
2018-10-16 15:57:23 -0400

    Updated Movie component route to render, and fixed save button function

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -27,7 +27,11 @@ export default class MovieList extends Component {
       <div className="movie-list">
         {this.state.movies.map(movie => (
           <Link to={`/movies/${movie.id}`}>
-            <MovieCard key={movie.id} movie={movie} />
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              addToSavedList={this.props.addToSavedList}
+            />
           </Link>
         ))}
       </div>
