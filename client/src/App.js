fdc4e37bbd2e8f719b7671ac1b18f8052f2f670b
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-10-16 20:55:06 -0700

    Added routes to MovieList and Movie components in App

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { homedir } from 'os';
 
 export default class App extends Component {
   constructor() {
@@ -23,7 +24,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route path='/' component={MovieList} />
+        <Route path='/movies/:id' component={Movie} />
       </div>
     );
   }
