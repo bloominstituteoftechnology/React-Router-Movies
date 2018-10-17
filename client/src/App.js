9e498337cfe2cc20ad11b3bba18db60baef774c3
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-10-16 20:44:57 -0700

    Wrapped App component in Router in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
