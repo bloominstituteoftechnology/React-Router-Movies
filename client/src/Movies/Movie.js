537185f89efb4599743ac2c9c99aaffb621dec0a
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-17 23:31:38 -0600

    Completed stretch goal: Saved movies are now links and fetch from the server

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -27,11 +27,11 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
@@ -46,22 +46,6 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        {/* <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div> */}
         <MovieCard movie={this.state.movie} />
         <div onClick={this.saveMovie} className="save-button">Save</div>
       </div>
