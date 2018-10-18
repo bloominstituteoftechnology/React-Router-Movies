91a0355681609d6ab3230d27058de3942e071857
BrandonMoll <brandonmoll85@gmail.com>
2018-10-17 21:14:51 -0500

    saved movies stretch goal complete

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} addToSavedList={this.addToSavedList} />
+        <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} /> } />
       </div>
     );
   }
