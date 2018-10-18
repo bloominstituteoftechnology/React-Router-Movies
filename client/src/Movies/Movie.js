d2c06744a98a8851082a0b51221bc1aaaabdff59
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-17 22:16:45 -0600

    Completed stretch goal: Implemented saveMovie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -33,10 +33,10 @@ export default class Movie extends Component {
   //   }
   // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  }
 
   render() {
     if (!this.state.movie) {
@@ -63,7 +63,7 @@ export default class Movie extends Component {
           ))}
         </div> */}
         <MovieCard movie={this.state.movie} />
-        <div className="save-button">Save</div>
+        <div onClick={this.saveMovie} className="save-button">Save</div>
       </div>
     );
   }
