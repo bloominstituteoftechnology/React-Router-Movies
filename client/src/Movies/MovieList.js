38148cc2b91013d204b679e91d909aeda7b914b8
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-10-17 22:43:20 -0400

    finish mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom'
+import MovieCard from './MovieCard.js'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ export default class MovieList extends Component {
   }
 
   render() {
-      console.log(movies);
+      console.log(this.state.movies);
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
@@ -36,21 +36,6 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movie/${movie.id}`} className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </Link>
+    <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
   );
 }
