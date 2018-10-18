bef3fb3719b9796648a30a500dbebc9dabdad9fa
Rick Fleming <elfeming111@yahoo.com>
2018-10-17 19:11:14 -0700

    Added save movie functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -44,12 +44,11 @@ export default class Movie extends Component {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
-    const { title, director, metascore, stars } = this.state.movie;
+    
     return (
       <div className="save-wrapper">
         <MovieCard {...this.state.movie}/>
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={this.saveMovie}>Save</div>
       </div>
     );
   }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,7 +35,6 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
     <Link to={`/movies/${movie.id}`}>
       <MovieCard {...movie}/>
