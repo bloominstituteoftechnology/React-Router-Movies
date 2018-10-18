61d18941a62e21042b1c281d8dbe0bfc5482d7b0
Kyle Teeter <kyleteeter12@gmail.com>
2018-10-17 22:25:44 -0500

    Part 2 done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import MovieCard from './Movies/MovieCard';
 
 export default class App extends Component {
   constructor() {
@@ -22,10 +23,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Route path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+
+        <div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} /> 
+        </div>
       </div>
     );
   }
