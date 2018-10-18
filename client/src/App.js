1854d006df1c8af100ddf68e9107df0d966715c7
marcusbass323 <marcus@bassconsultantgroup.com>
2018-10-17 22:23:13 -0700

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
+//import all here
+
 export default class App extends Component {
   constructor() {
     super();
