32d1d8e87e3a307a7e42640150b84dd004ee0f9e
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-17 19:43:40 -0600

    Made cards links and displays correct card on click

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MoviesList} />
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={Movie} />
       </div>
     );
