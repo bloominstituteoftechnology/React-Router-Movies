30ab2a7b245d521935e07d8bb9b3bdcd2cd8d359
Abigayle Franklin <abifanklin@gmail.com>
2018-10-17 21:27:48 -0500

    Routes in place

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -39,7 +39,7 @@ export default class Movie extends Component {
 
   render() {
     if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
+      return <div>Hi!Loading movie information...</div>;
     }
 
     const { title, director, metascore, stars } = this.state.movie;
