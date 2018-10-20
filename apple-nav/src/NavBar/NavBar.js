b44039746202f2a170bddd87acf4b5c45395e80f
BrandonMoll <brandonmoll85@gmail.com>
2018-10-20 12:59:49 -0500

    Built out some structure of the apple-nav

---
+++ apple-nav/src/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                I am a Nav Bar
+            </div>
+        )
+    }
+};
+
+export default NavBar;
