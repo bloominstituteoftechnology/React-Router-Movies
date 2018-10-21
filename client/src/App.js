5c715fcdb177f8bca6493b6e2fb29c30a0557aa5
vy3191 <venkyafhs@gmail.com>
2018-10-20 22:58:45 -0400

    Completed part 1

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,9 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        // <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movie/:id" component={Movie} />
       </div>
     );
   }
