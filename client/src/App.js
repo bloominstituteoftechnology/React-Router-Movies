0a60560ac88dc504869fece0d1f250bb3d39707a
Phil Hart <phil@Phils-iMac.local>
2018-10-22 15:43:26 -0600

    initital commit and finished part 1

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <div>
+          <Route exact path='/' component={MovieList}> </Route>
+          <Route exact path='/movies/:id' component={movie}></Route>
+        </div>
       </div>
     );
   }
