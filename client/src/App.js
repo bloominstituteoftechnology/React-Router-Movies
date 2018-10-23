22eb4d1cb994207b5d6729a7995ad5ca9c08cdf7
Phil Hart <phil@Phils-iMac.local>
2018-10-23 16:10:32 -0600

    Almost Finished, working on moviecard now

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -24,7 +24,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>
           <Route exact path='/' component={MovieList}> </Route>
-          <Route exact path='/movies/:id' component={movie}></Route>
+          <Route path='/movies/:id' component={Movie}> </Route>
         </div>
       </div>
     );
