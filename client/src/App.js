15bc8e2f895337aa80c9e1189ea4fb641bbee50a
mikaelacurrier <mikaela.currier@gmail.com>
2018-10-23 14:37:32 -0400

    refactored to include render instead of component. also got save movie button functional

--- client/src/App.js
+++ client/src/App.js
@@ -23,11 +23,9 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>
-          <Route exact path='/' component={MovieList}></Route>
-          <Route path='/movies/:id' component={Movie}></Route>
+          <Route exact path='/' component={MovieList} />
+          <Route path='/movies/:id' render={props => {return <Movie {...props} addToSavedList={this.addToSavedList} />}} />
         </div>
-      </div>
     );
   }
 }
