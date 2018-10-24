0a895c5f62de4b2e4a75f6092ff6e4be1b90011e
Alex Ostrander <alexanderostrander23@gmail.com>
2018-10-24 00:10:08 -0400

    Finished most of part two, creating home function now for saved list

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Movie from './Movie.js'
+import { Link } from 'react-router-dom'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -34,11 +36,12 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
+     <Link to={`/movies/${movie.id}`}>
+      <div className="movie-card">
+       <h2>{title}</h2>
+       <div className="movie-director">
         Director: <em>{director}</em>
-      </div>
+       </div>
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
@@ -49,6 +52,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+     </div>
+    </Link>
   );
 }
