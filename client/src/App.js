dcecc503a68e1607c0703acb1a83d27a6566e3f3
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-23 23:02:16 -0400

    Added necessary dependencies, imported BrowserRouter & Route to their respective component files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
@@ -20,10 +21,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
+
+<Route exact path='/home'>
+  <div>  
+  <SavedList list={this.state.savedList} />
+  <div>Replace this Div with your Routes</div>
+  </div>
+</Route>
+      
     );
   }
 }
