58ebbb63f1c67783da474de4bb4ac8e24815727a
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-24 23:11:48 -0400

    Working on stretch goal to refactor code to DRY.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import axios from 'axios'
 
-const MovieCard = props => {
-  return;
-}; 
 
+class MovieCard extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      movies: []
+    }
+
+  }
+
+  componentDidMount(){
+    axios
+    .get('http://localhost:5000/api/movies')
+    .then(response => {
+      this.setState(() => ({movies: response.data}))
+    })
+    .catch(error => {
+      console.error('Server Error', error)
+    })
+  }
+
+  function = MovieDetails({ movie }){
+    const {title, director, metascore, stars} = movie
+  }
+  render(){
+    return(
+      <div>
+      {this.state.movies.map((movie, index) => <div key={index}>
+      {movie.title}
+      {movie.movie}
+      </div>)}
+      </div>
+    )
+  }
+}
 export default MovieCard;
