5555c269b375dad01e80466546f8c006089f39c5
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-24 22:24:09 -0400

    Added functionality for traveling home.

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,6 @@ export default class App extends Component {
     return (
 
   <div>  
-  <Link to='/' >Movie</Link>
   <SavedList list={this.state.savedList} />
   {/* <div>Replace this Div with your Routes</div> */}
   <Route exact path='/' component={MovieList}
