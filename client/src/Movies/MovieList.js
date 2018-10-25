1fe5bc8a25a3940cd9fba32c40f9d98c5a939aec
Jesse <theiseja@gmail.com>
2018-10-25 02:22:49 -0500

    Completed Part 1 & 2. Starting stretch tomorrow

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -35,21 +37,6 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <Link to={`/movies/${movie.id}`}>{title}</Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <MovieCard {...movie} />
   );
 }
