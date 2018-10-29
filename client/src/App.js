82428d954a64bfe63869b6293c9d994ab9387d1e
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-10-29 16:43:32 -0400

    (PART 2) - made MovieList a link

--- client/src/App.js
+++ client/src/App.js
@@ -22,9 +22,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>
           <Route exact path="/" component={MovieList} />
-        </div>
+          <Route path="/movies/:id" render={ (props) => {
+          return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
+          }} />
       </div>
     );
   }
