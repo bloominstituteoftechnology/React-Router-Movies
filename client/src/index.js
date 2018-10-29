5bd5c93ca2ce4d4baaf112f58d7df17fb6f63b56
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-10-28 22:28:38 -0400

    (PART 1) - created Route for home path

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -9,4 +9,4 @@ ReactDOM.render(
         <App />
     </Router>,
     document.getElementById("root")
-);
+); 
