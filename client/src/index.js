04248e28585283dea60585f78741f524d7a0166a
Demetri Bethel <demetribethel@gmail.com>
2018-11-03 13:25:31 -0400

    Added 2 routes, including the Movie Components.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 import './index.css';
 import App from './App';
@@ -8,5 +10,13 @@ import App from './App';
 ReactDOM.render(
 <Router>
 <App />
+
+<div> 
+ <Route exact path="/" component={MovieList}/>
+ <Route path="/movies/:id" component={Movie}/>
+ </div>
+
 </Router>,
  document.getElementById('root'));
+
+
