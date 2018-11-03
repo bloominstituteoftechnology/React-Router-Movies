eb759b5bec7ab79c3dbef271a6e4d8d3516694d3
Demetri Bethel <demetribethel@gmail.com>
2018-11-03 15:03:45 -0400

    corrected routes to be included on app,js

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
 <Router>
-
-
-<div> 
- <Route exact path="/" component={MovieList}/>
- <Route path="/movies/:id" component={Movie}/>
  <App />
- </div>
-
-</Router>,
+ </Router>,
  document.getElementById('root'));
 
 
