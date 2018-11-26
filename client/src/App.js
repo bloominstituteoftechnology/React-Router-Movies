c18cdf3b7d3b1bd9e649300b91eaac03ee7309f1
turkworks <cengizbug@Cengizs-iMac.local>
2018-11-26 16:50:47 -0700

    almost finished with movies mvp

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import {Route} from 'react-router-dom';
+
+
+
+
 
 export default class App extends Component {
   constructor() {
@@ -19,10 +24,12 @@ export default class App extends Component {
   };
 
   render() {
+    const { savedList } = this.state;
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" render={props => <MovieList {...props} savedList={savedList}/> } />
+        <Route path="/movies/:id" component={Movie} />
       </div>
     );
   }
