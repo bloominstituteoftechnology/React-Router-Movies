81ee43e0619ce00f8b236a4fdcc97835c06bf5b8
Chaya Otikor <c.otikor@gmail.com>
2018-11-26 15:01:11 -0700

    Finished most of the stretch, working on making code DRY

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -10,10 +10,10 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map((movie, index) => (
-         <Link to={`/movies/${movie.id}`}><span className="saved-movie" key={index}>{movie.title}</span></Link>
+         <NavLink to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: 'black'}} activeStyle={{fontWeight: 'bold', color: 'lightseagreen'}}><span className="saved-movie" key={index}>{movie.title}</span></NavLink>
         ))}
         
-        <Link to='/'>Home</Link>
+        <NavLink to='/' style={{textDecoration: 'none', color: 'black'}}>Home</NavLink>
         
       </div>
     );
