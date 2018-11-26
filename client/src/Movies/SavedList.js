ac863e9ab67bec7c57b13a942673136692ff93fd
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-26 15:48:24 -0600

    add styting to index.css

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,11 +11,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-            <Link key={movie.id} to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+            <NavLink key={movie.id} to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</NavLink>
         ))}
-        <Link to="/">
-          <div className="home-button">Home</div>
-        </Link>
+        <NavLink exact to="/" className="saved-movie">
+          <div>Home</div>
+        </NavLink>
       </div>
     );
   }
