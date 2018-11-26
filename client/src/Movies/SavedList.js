d4e9d966d30df2bc68f293d60c66ad6c04cc4327
Chaya Otikor <c.otikor@gmail.com>
2018-11-26 15:48:06 -0700

    Finished making code DRY and changed styling

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -8,12 +8,14 @@ export default class SavedList extends Component {
   render() {
     return (
       <div className="saved-list">
-        <h3>Saved Movies:</h3>
+      <h3>Saved Movies</h3>
+      <div className= 'saved'>
+        
         {this.props.list.map((movie, index) => (
          <NavLink to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: 'black'}} activeStyle={{fontWeight: 'bold', color: 'lightseagreen'}}><span className="saved-movie" key={index}>{movie.title}</span></NavLink>
         ))}
-        
-        <NavLink to='/' style={{textDecoration: 'none', color: 'black'}}>Home</NavLink>
+       </div> 
+        <NavLink to='/' style={{textDecoration: 'none', color: 'black', fontWeight: 'bold', borderBottom: '1px solid black'}}>Home</NavLink>
         
       </div>
     );
