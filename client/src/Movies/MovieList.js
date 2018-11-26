f625782b512a1c96b68663f66078313be258a41c
Chaya Otikor <c.otikor@gmail.com>
2018-11-26 15:50:21 -0700

    Used prettier to clean up code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,37 +1,39 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: []
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:5000/api/movies')
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('http://localhost:5000/api/movies')
+			.then((response) => {
+				this.setState(() => ({ movies: response.data }));
+			})
+			.catch((error) => {
+				console.error('Server Error', error);
+			});
+	}
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <Link to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: '#F2F6D0'}}>
-          <MovieCard key={movie.id} movie={movie} />
-          </Link>
-        ))}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="movie-list">
+				{this.state.movies.map((movie) => (
+					<Link
+						to={`/movies/${movie.id}`}
+						style={{ textDecoration: 'none', color: '#F2F6D0' }}
+					>
+						<MovieCard key={movie.id} movie={movie} />
+					</Link>
+				))}
+			</div>
+		);
+	}
 }
-
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div className="saved-list">
-      <h3>Saved Movies</h3>
-      <div className= 'saved'>
-        
-        {this.props.list.map((movie, index) => (
-         <NavLink to={`/movies/${movie.id}`} style={{textDecoration: 'none', color: 'black'}} activeStyle={{fontWeight: 'bold', color: 'lightseagreen'}}><span className="saved-movie" key={index}>{movie.title}</span></NavLink>
-        ))}
-       </div> 
-        <NavLink to='/' style={{textDecoration: 'none', color: 'black', fontWeight: 'bold', borderBottom: '1px solid black'}}>Home</NavLink>
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="saved-list">
+				<h3>Saved Movies</h3>
+				<div className="saved">
+					{this.props.list.map((movie, index) => (
+						<NavLink
+							to={`/movies/${movie.id}`}
+							style={{ textDecoration: 'none', color: 'black' }}
+							activeStyle={{ fontWeight: 'bold', color: 'lightseagreen' }}
+						>
+							<span className="saved-movie" key={index}>
+								{movie.title}
+							</span>
+						</NavLink>
+					))}
+				</div>
+				<NavLink
+					to="/"
+					style={{
+						textDecoration: 'none',
+						color: 'black',
+						fontWeight: 'bold',
+						borderBottom: '1px solid black'
+					}}
+				>
+					Home
+				</NavLink>
+			</div>
+		);
+	}
 }
