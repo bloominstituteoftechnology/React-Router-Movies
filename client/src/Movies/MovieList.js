102bce4c120fccddbeae412b1fa64dc6b6ddbef0
Trai Lynne <trailynne@gmail.com>
2018-11-26 18:13:41 -0700

    Refactored code to make it more DRY Moved movie details into MovieCard Rendered MovieCard in MovieList and Movie

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+import MovieCard from './MovieCard';
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -26,7 +28,7 @@ export default class MovieList extends Component {
       <div className="movie-list">
         {this.state.movies.map(movie => (
           <Link to={`/movies/${movie.id}`}>
-            <MovieDetails key={movie.id} movie={movie} />
+            <MovieCard key={movie.id} movie={movie} />
           </Link>
         ))}
       </div>
