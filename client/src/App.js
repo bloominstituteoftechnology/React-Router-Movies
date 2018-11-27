f83d5e0edffe332f3f4217e8b47a54e0a0a128d1
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-11-27 13:58:43 -0800

    Add save movie functionality

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path='/' component={MovieList}/>
-        <Route path='/movies/:id' render={(props)=> <Movie {...props} />}/>
+        <Route path='/movies/:id' render={(props)=> <Movie {...props} addToSavedList={this.addToSavedList} list={this.state.savedList}/>}/>
       </div>
     );
   }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -28,26 +28,27 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    //items.find(item => `${item.id}` === props.match.params.itemId);
+    if (!this.props.list.find((movie) => movie === this.state.movie))
+      addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-    //const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={this.saveMovie}>Save</div>
       </div>
     );
   }
