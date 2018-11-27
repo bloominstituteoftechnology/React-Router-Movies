22d32ff9a6f8599e061efdbf6534cab72204b187
Thomas Claydon <tomclaydon102@gmail.com>
2018-11-27 16:51:04 -0500

    Submission 3 - Stretch Goal - Refactor so that our code is Dry

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -33,10 +34,15 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+
+  // const { title, director, metascore, stars, id } = movie;
+  
+  const {id} = movie;
   return (
     <Link to={`/movies/${id}`}>
-      <div className="movie-card">
+      <MovieCard movie={movie}/>
+
+      {/* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -51,7 +57,8 @@ function MovieDetails({ movie }) {
             {star}
           </div>
         ))}
-      </div>
+      </div> */}
+
     </Link>
   );
 }
