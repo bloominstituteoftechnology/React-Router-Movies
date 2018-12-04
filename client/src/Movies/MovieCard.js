56aeaf3f8cb62caca758bd434ecaef1d08383e4a
chrisgins <christiansgina@gmail.com>
2018-12-04 11:35:14 -0700

    small changes complete

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props;
+  return (
+    <div className="movie">
+      <h2>{title}</h2>
+      <div className="director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+      {stars.map(star => (
+        <div key={star} className="star">
+          {star}
+        </div>
+      ))}
+      <div className="save-button" onClick={props.saveMovie}>Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-
+import { Link, NavLink } from "react-router-dom";
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+
 
   render() {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`} key={movie.id} className='link'>
+            <span  className="saved-movie">{movie.title}</span>
+            </NavLink>
         ))}
-        <div className="home-button">Home</div>
+        <Link to='/'className="home-button">Home</Link>
       </div>
     );
   }
