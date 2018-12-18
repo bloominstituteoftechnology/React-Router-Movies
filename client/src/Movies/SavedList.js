0031ab91bc71b8bb846fb23bcffd361d3b167566
edreeseg <edward.reeseg@gmail.com>
2018-12-18 09:36:12 -0500

    NavLink saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default props => {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+          <NavLink to={`/movies/${movie.id}`} 
+          className="saved-movie"
+          activeClassName="active-saved-movie">{movie.title}</NavLink>
         ))}
         <Link to="/"><div className="home-button">Home</div></Link>
       </div>
