de5b0b30d5ee4c7059e36f4d6f564ff48bd56cb6
dickwinters <coding1965@gmail.com>
2019-01-16 00:20:23 -0600

    implemented selection of single move and back to home screen through use of routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import {Route} from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      savedList: []
+      savedList: [],
     };
   }
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
     savedList.push(movie);
-    this.setState({ savedList });
+    this.setState({savedList});
   };
 
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList}>
+          List Placeholder
+        </Route>
+        <Route path="/movies/:id" component={Movie}>
+          Movie Placeholder
+        </Route>
       </div>
     );
   }
