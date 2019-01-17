815391f2ced3c52de2c4ec9c60a83314e6b0058f
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2019-01-17 18:31:46 -0500

    Part 1 Readme complete. Movies Render

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+
+//Part 1
+//* Inside your App file add two routes.
+// * one route for `/` that loads the `MovieList` component.
+// * one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component.
 
 export default class App extends Component {
   constructor() {
@@ -22,7 +28,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList}/>
+        <Route path="/movies/:id" render={props=>(
+          <Movie {...props} addToSavedList={this.addToSavedList}/>
+        )}/>
       </div>
     );
   }
