f7552d8d80b69b9d3c4c64e5c1261d819f7b5082
jrizza88 <jetorres88@gmail.com>
2019-01-16 22:59:21 -0500

    clened up some code in Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -43,11 +43,11 @@ export default class Movie extends Component {
       return <div>Loading movie information...</div>;
     }
 
-    const { title, director, metascore, stars } = this.state.movie;
+    // const { title, director, metascore, stars } = this.state.movie;
     return (
   
       <div className="save-wrapper">
-      <MovieCard movie={this.movie} />
+      <MovieCard movie={this.state.movie} />
         {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
