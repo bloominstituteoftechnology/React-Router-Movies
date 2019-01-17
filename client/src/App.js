aa579c60d8f3bef83000e379377080392d77140e
Kseniya Platonava <ksu2008@gmail.com>
2019-01-16 22:18:58 -0500

    Part 2
    Add URL to MovieList
    Add functionality to Home button

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        
         <div className="App">
           <Route path="/" component={MovieList} />
           <Route path="/movies/:id" component={Movie} />
