34b4dea660671f111181f4f22243325b5737df44
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-01-16 21:26:48 -0700

    stretch: dds Link functionlity to saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MovieList from './MovieList';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,7 +12,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+          <Link to={`/movies/${movie.id}`}>
+            <span className="saved-movie">{movie.title}</span>
+          </Link>
         ))}
         <Link to="/" className="home-button">Home</Link>
       </div>
