d21ad8d48d8c254a8f5856dca835a96f71d15a50
Jonathan Dorety <kyullog@gmail.com>
2019-01-17 11:10:56 -0500

    Refactor SavedList into functional component
    Yay no React flags!

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,26 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  z;
-
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
-            <span className="saved-movie">{movie.title}</span>
-          </Link>
-        ))}
-        <Link to={"/"} className="home-button">
-          Home
+const SavedList = props => {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+          <span className="saved-movie">{movie.title}</span>
         </Link>
-      </div>
-    );
-  }
-  z;
-}
+      ))}
+      <Link to={"/"} className="home-button">
+        Home
+      </Link>
+    </div>
+  );
+};
+
+export default SavedList;
