98d6faf31a1c436f55ec27aaf1c38b5d1e8968d7
kingmetri <demetribethel@gmail.com>
2019-01-16 20:28:40 -0500

    Set up Router.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'; //Import Router
 import './index.css';
 import App from './App';
+//Wrap the App with Router
+//Always wrap the index.js wrap.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+                    <App /> 
+                    </Router>, 
+                                document.getElementById('root'));
