6c6d7b65d9a2b08abc6199786c0f48527cb4704c
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-01-16 23:26:26 -0500

    REFACTOR MOVIECARD COMPONENT

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import { Link } from 'react-router-dom';
-
+import MovieCard from './MovieCard';
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -26,7 +26,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+          <MovieDetails movie={movie} />
+        </Link> 
         ))}
       </div>
     );
@@ -37,22 +39,7 @@ function MovieDetails({ movie }) {
   const { id, title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <Link to={`/movies/${id}`}>
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </Link> 
+        <MovieCard title={title} director={director} metascore={metascore} stars={stars}/>
     </div>
   );
 }
