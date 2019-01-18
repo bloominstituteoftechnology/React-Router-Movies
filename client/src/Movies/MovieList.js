83b8a0f6ce52c453e9002128f7645ba1f5ae3c09
Kseniya Platonava <ksu2008@gmail.com>
2019-01-18 17:24:18 -0500

    Stretch Goal 3 and 4
    Turn Saved Movies to Links
    
    Turn Links to NavLinks

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -34,7 +34,6 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
     < Link to={`/movies/${movie.id}`}>
     <MovieCard key={movie.id} movie={movie} /> 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 class SavedList extends Component {
   constructor(props) {
@@ -11,9 +11,9 @@ class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+          <NavLink activeClassName="saved-active" to={`/movies/${movie.id}`} className="saved-movie">{movie.title}</NavLink>
         ))}
-        < Link to="/">Home</Link>
+        < NavLink exact to="/">Home</NavLink>
       </div>
     );
   }
