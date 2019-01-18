297084da564e06376613207cc15a8073a0c3add2
120356aa <yurlovandrew@gmail.com>
2019-01-18 15:08:59 -0800

    refractored

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+// import styled from 'styled-components';
 
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
