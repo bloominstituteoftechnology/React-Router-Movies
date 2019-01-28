c80fff93f308a1bae90397642d3a265d6c1cfa94
Clint Kunz <clintkunz@gmail.com>
2019-01-28 14:37:25 -0700

    add netlify file

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,7 +11,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <NavLink className="saved-movie" to={`/movie/${movie.id}`} className="saved-movie">{movie.title}</NavLink>
+          <NavLink className="saved-movie" to={`/movie/${movie.id}`} className="saved-movie">
+          {/* {if(movie.title.length > 20) movie.title.substring(0, 20)} */}
+          {movie.title.length > 20 ? movie.title.substring(0,20)+ '...' : movie.title  }
+
+          </NavLink>
         ))}
         <div>
           <div onClick={() => this.props.history.push('/')} className="home-button">Home</div>
