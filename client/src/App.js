37229cb6f416c2c0867ff7a66dc0e6e9287ad48a
mustafa inaya <moose.inaya@icloud.com>
2019-01-30 18:03:21 -0800

    RENDER, Rednering Data By Using React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,32 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			savedList: []
+		};
+	}
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+	addToSavedList = (movie) => {
+		const savedList = this.state.savedList;
+		savedList.push(movie);
+		this.setState({ savedList });
+	};
 
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<Route exact path="/" component={MovieList} />
+					<Route exact path="/movies/:id" component={Movie} />
+				</div>
+				<SavedList list={this.state.savedList} />
+				{/* <div>Replace this Div with your Routes</div> */}
+			</div>
+		);
+	}
 }
