29092ee566310a5531e1dca951b9747f4eb6f0cf
thomasutsey <tomutsey@yahoo.com>
2019-02-04 22:30:04 -0500

    Reconfigure the MovieList card to reflect the addition of the MovieCard component
    The MovieList component No longer requires that it hand rendering an output but simply determines what should direct the Movie Card to render
    
    Pass the determined request as props to the MovieCard

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -27,11 +27,7 @@ export default class MovieList extends Component {
      
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          
-          <Link to={`/movies/${movie.id}`}>
           <MovieDetails key={movie.id} movie={movie} />
-          </Link>
-
         ))}
       </div>
     );
@@ -39,23 +35,10 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { id } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+    <NavLink to={`/movies/${id}`}>
+      <MovieCard movie={movie}/>
+    </NavLink>
+      )
 }
