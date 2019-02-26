d303da9102f76d279e6b00b0ccfab28ebe0e0523
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-02-26 15:33:39 -0800

    MVP complete (I think) was having problem with LocalHost:5000 and would not pull the API data

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,44 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { BrowserRouter as Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      savedList: []
+      savedList: [],
+      movieInList: null
     };
   }
-
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    savedList.push(movie);
+    const findMovie = savedList.find(el => movie.id === el.id);
+    if (findMovie) {
+      this.setState({ movieInList: `You've already saved that movie!` });
+      setTimeout(() => this.setState({ movieInList: null }), 2000);
+    } else {
+      savedList.push(movie);
+    }
+
     this.setState({ savedList });
   };
 
   render() {
+    const { movieInList } = this.state;
     return (
       <div>
+        {movieInList !== null ? (
+          <h3 className="movie-warning">{movieInList}</h3>
+        ) : null}
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={props => (
+            <Movie {...props} addToSavedList={this.addToSavedList} />
+          )}
+        />
       </div>
     );
   }
