a3d0ebb45906db50b4781e2c8b2ca8e2fbd7d309
Chris Dalao <chrisdalao@gmail.com>
2019-06-10 14:00:29 -0700

    push correction to personal branch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,7 +11,6 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
-    console.log(this.props.match.params.movieId);
     const id = this.props.match.params.movieId;
     this.fetchMovie(id);
   }
