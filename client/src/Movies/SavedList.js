041b241f4160784939310af0c50197477e97d9e5
Andrew Brudnak <brudnak@icloud.com>
2019-06-10 15:17:55 -0700

    Set up search links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,10 +5,13 @@ const SavedList = ({ list }) => {
   return (
     <div className='saved-list'>
       <h3>Saved Movies:</h3>
+
       {list.map(movie => (
-        <span key={movie.id} className='saved-movie'>
-          {movie.title}
-        </span>
+        <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <span key={movie.id} className='saved-movie'>
+            {movie.title}
+          </span>
+        </Link>
       ))}
       <Link to={'/'}>
         <div className='home-button'>Home</div>
