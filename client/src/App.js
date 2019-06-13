f2f0707d7281e6af984363833bfc639d9b0313ac
ckopecky <kopecky12112@gmail.com>
2019-06-13 09:58:07 -0700

    update project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+
 import SavedList from './Movies/SavedList';
-import List from './Movies/List';
+import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 export default class App extends Component {
@@ -14,25 +14,15 @@ export default class App extends Component {
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
-    let found = false;
-    
-    savedList.forEach(savedMovie => {
-      if (savedMovie.id === movie.id){
-        found = true;
-      }
-    });
-
-    if(!found){
-      savedList.push(movie);
-      this.setState({ savedList });
-    }
+    savedList.push(movie);
+    this.setState({ savedList });
   };
+
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path = "/" component = {List}/>
-        <Route exact path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={this.addToSavedList}/>}></Route>
+        <div>Replace this Div with your Routes</div>
       </div>
     );
   }
