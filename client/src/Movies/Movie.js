31f228fa3c9b98c8b531bf1c54e9039f73a0c9f9
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-06-19 15:27:07 -0500

    functionality to movie card & home button added

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -9,9 +9,10 @@ export default class Movie extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount(props) {
     // change this line to grab the id passed on the URL
     const id = 1;
+    console.log(id)
     this.fetchMovie(id);
   }
 
