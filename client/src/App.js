f27eee9520fee87fc3ddc0f5ec6c66050ccce76b
Matt M <matt.d.m@hotmail.com>
2019-08-19 17:50:09 -0500

    mvp + stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route path='/' component={MovieList} exact/>
+        <Route path='/movies/:id' render={props => <Movie id={props.match.params.id} addToSavedList={addToSavedList}/>}/>
+      </div>
     </div>
   );
 };
