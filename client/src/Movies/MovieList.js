c8600158386ebfb4fb0c4f96206825e7bacee5a8
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-20 13:53:30 -0400

    refactored to DRY code

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 import axios from "axios";
 
 import { Link } from "react-router-dom";
@@ -31,26 +32,7 @@ const MovieList = props => {
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <Link to={`movies/${movie.id}`}>
-        <h2>{title}</h2>
-      </Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+  return <MovieCard movie={movie} />;
 }
 
 export default MovieList;
