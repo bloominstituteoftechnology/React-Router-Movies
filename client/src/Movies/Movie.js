4d62093736f8f03c510ba39a5a7371aa0ad17b3e
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-20 14:03:27 -0400

    added save functionality with navlinks

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -21,17 +21,24 @@ const Movie = props => {
   }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
 
   const { title, director, metascore, stars } = movie;
-  return <MovieCard movie={movie} />;
+  return (
+    <div className="save-wrapper">
+      <MovieCard movie={movie} />
+      <div className="save-button" onClick={saveMovie}>
+        Save
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <NavLink
+        to={`/movies/${movie.id}`}
+        className="saved-movie"
+        activeClassName="saved-active"
+      >
+        {movie.title}
+      </NavLink>
     ))}
-    <Link className="home-button" to="/">
+    <NavLink className="home-button" to="/">
       Home
-    </Link>
+    </NavLink>
   </div>
 );
 
