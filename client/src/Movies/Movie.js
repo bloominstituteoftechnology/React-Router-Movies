344b3f9cce3491585bda92c7c9bc609cf685a0b9
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-16 22:30:23 +0000

    complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -20,13 +20,13 @@ const Movie = props => {
       .catch(error => {
         console.error(error);
       });
-  }, []);
+  }, [params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  };
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -52,7 +52,9 @@ const Movie = props => {
             </div>
           ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>
+        Save
+      </div>
     </div>
   );
 };
