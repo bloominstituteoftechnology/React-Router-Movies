cec65d9f966531611561e29b5d509550ff514bbf
Chris Daley <Daleys@Chriss-iMac.local>
2019-10-04 22:18:30 -0600

    finished one stretch goal

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MovieList from "./MovieList";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+
   useEffect(() => {
-    const id = props.match.params.id;
+    const id = Number(props.match.params.id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -30,29 +32,30 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+  return <Route  path ="/" render={props => <MovieCard movie={movie} {...props} /> } />
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+  // const { title, director, metascore, stars } = movie;
+  // return (
+  //   <div className="save-wrapper">
+  //     <div className="movie-card">
+  //       <h2>{title}</h2>
+  //       <div className="movie-director">
+  //         Director: <em>{director}</em>
+  //       </div>
+  //       <div className="movie-metascore">
+  //         Metascore: <strong>{metascore}</strong>
+  //       </div>
+  //       <h3>Actors</h3>
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  //       {stars.map(star => (
+  //         <div key={star} className="movie-star">
+  //           {star}
+  //         </div>
+  //       ))}
+  //     </div>
+  //    
+  //   </div>
+  // );
 }
 
 export default Movie;
