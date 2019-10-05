8d778f3588648624d62d703db4abfee8a3853403
Chris Daley <Daleys@Chriss-iMac.local>
2019-10-04 19:58:28 -0600

    finished MVP will submit a pull request and work on stretch now.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Movie from "./Movie";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -31,7 +33,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={`/movies/${movie.id}`}> <h2>{title}</h2> </Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
@@ -43,7 +45,7 @@ function MovieDetails({ movie }) {
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
-        </div>
+        </div>        
       ))}
     </div>
   );
