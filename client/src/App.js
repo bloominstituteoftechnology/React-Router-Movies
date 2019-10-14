101069b25ba7adb8d247b8edb36342257423d733
Forrest-Bingham <bingham_5@live.com>
2019-10-14 17:34:57 -0600

    finished mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+import { Route } from "react-router-dom";
+import MovieCard from "./Movies/MovieCard";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/Movie/:MovieID" component={Movie} />
     </div>
   );
 };
