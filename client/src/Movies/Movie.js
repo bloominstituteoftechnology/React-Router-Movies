828fe68df15b962a978b8b824484d6de4bdebe58
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-14 21:40:24 -0500

    set up BrowserRouter in index.js, imported Links, Route paths to App.js, and rendered movie cards

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -9,6 +9,7 @@ const Movie = (props) => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
+
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+       <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
@@ -30,7 +31,9 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+  
+   <div className="movie-card">
+
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -43,9 +46,13 @@ function MovieDetails({ movie }) {
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
+
         </div>
+        
       ))}
+     
     </div>
+   
   );
 }
 
