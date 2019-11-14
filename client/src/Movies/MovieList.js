e86753e91af645c84c059199fd39635332acf610
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2019-11-14 14:36:47 +0100

    movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -17,19 +18,24 @@ const MovieList = props => {
     
     getMovies();
   }, []);
-  
+  console.log(movies);
   return (
+    
+  <Link to=' /movies/${movie.id}'>
+
+
     <div className="movie-list">
       {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
-      ))}
+        ))}
     </div>
-  );
+    </Link> );
 }
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+   
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +52,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+  
   );
 }
 
