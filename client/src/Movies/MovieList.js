825d005f2aea07f6906dad79582ef90f24c2202f
Tony Walker <Walkertony509@gmail.com>
2019-12-10 19:03:41 -0800

    I have completed MVP.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
-          setMovies(response.data);
+          this.setState(() => ({ movies: response.data }));
         })
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
-    
-    getMovies();
-  }, []);
+  }
   
-  return (
+  render () {
+    return (
     <div className="movie-list">
-      {movies.map(movie => (
+      {this.state.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
-}
+  }
+  }
+  
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <Link to={`/movies/${movie.id}`}>
+			<MovieCard
+				title={title}
+				director={director}
+				metascore={metascore}
+				stars={stars}
+				addToSaved={movie.addToSaved}
+			/>
+		</Link>
   );
 }
 
-export default MovieList;
