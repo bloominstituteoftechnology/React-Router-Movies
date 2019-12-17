d993a0ef817f86cb1d53a78b3fd0cad8ea4f000b
Kwang Woo Lee <kl467@cornell.edu>
2019-12-18 08:18:28 +0900

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+// Import Dependencies
 import React, { useState } from 'react';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
+// Import Components
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,9 +18,24 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <MovieList />
+          </Route>
+          <Route path="/movies/:id">
+            <Movie />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
 
 export default App;
+
+
+{/* <nav>
+<NavLink to="/">MovieList</NavLink>
+<NavLink to="/movies/">Movie</NavLink>
+</nav> */}
