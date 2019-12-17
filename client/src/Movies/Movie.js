f5473caabd135a033076e89f20e8e9d36469f925
Kwang Woo Lee <kl467@cornell.edu>
2019-12-18 08:35:38 +0900

    Stretch 1/4

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
-  const {id} = useParams();
-  
+  let {id} = useParams();
+
   useEffect(() => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
@@ -18,7 +19,6 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-
   },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
@@ -30,29 +30,10 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
-  const { title, director, metascore, stars } = movie;
   
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-          <Link to={`/movies/${movie.id}`}>
-            <h2>{title}</h2>
-          </Link>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie}/>
       <div className="save-button">Save</div>
     </div>
   );
