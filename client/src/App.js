abcdf4e75e7848d1d7e88e996d032269a40b4847
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-16 19:58:59 -0700

    imported MovieList and route/link into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Route, Link } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
 
@@ -12,7 +13,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+
+
     </div>
   );
 };
