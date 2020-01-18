cf25961b9e27cc096ddd5579097bd8405c322c04
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-01-18 14:11:34 -0800

    MVP + Styling added

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -7,9 +8,9 @@ const MovieList = props => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
-        .then(response => {
-          console.log(response.data);
-          setMovies(response.data);
+        .then(res => {
+          console.log(res.data);
+          setMovies(res.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -21,7 +22,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
