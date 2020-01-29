e262a5fbb8ff4337ebd65c6f73d989e37b5fb081
mastaryan <sizemore.ryan@gmail.com>
2020-01-29 11:57:46 -0500

    Movies Content updated

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from "./MovieCard";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -13,7 +14,7 @@ const MovieList = props => {
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
+    };
     
     getMovies();
   }, []);
@@ -21,7 +22,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
   );
