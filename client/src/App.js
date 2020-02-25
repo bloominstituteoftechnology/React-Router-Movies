2d8a2ed310a288e84d0b89d5f0db914c78c19a8d
kevin lautermilch <kevin.laut@outlook.com>
2020-02-25 18:05:14 -0500

    :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-
+import {Route, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
-
+  const [id, setId] = useState();
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
@@ -12,7 +14,17 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+
+        <Switch>
+        <Route exact path='/'><MovieList set={setId}/></Route>
+        <Route path={`/movies/${id}`}>
+        <Movie key={id} id={id}/> 
+        </Route>
+
+</Switch>
+
+      </div>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -14,38 +15,42 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
     getMovies();
   }, []);
-  
+  // console.log(props.set)
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie} set={props.set}/>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
+function MovieDetails(props) {
+  // const { title, director, metascore, stars, id} = movie;
+
+  return ( 
+
     <div className="movie-card">
-      <h2>{title}</h2>
+        <Link to={`/movies/${props.movie.id}`} onClick={() => {props.set(props.movie.id)}}>
+      <h2>{props.movie.title}</h2>
       <div className="movie-director">
-        Director: <em>{director}</em>
+        Director: <em>{props.movie.director}</em>
       </div>
       <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+        Metascore: <strong>{props.movie.metascore}</strong>
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {props.movie.stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
       ))}
+        </Link>  
     </div>
+
   );
 }
 
