8bd8629fd2a4aef7cee84b48ccf4dda06e6a0681
Darrell-Conklin <dmconklin84@gmail.com>
2020-03-11 19:53:22 -0500

    installed dependencies for both servers, imported router, wrapped App, created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie.js';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +13,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      <Route exact path="/" />
+      <Route path="/movies/${id}" component={Movie} />
     </div>
   );
 };
