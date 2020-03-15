5f6737155ece9c2236a48d551931354f3ea5ea89
Matthew Castillo <blacksheep3y@gmail.com>
2020-03-15 14:17:37 -0400

    finished PART 1 and PART 2 DONE 100%

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import MovieCard from "./MovieCard";
-
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
@@ -28,14 +26,29 @@ const Movie = (props) => {
   //   addToSavedList(movie)
   // }
 
-  // if (!movie) {
-  //   return <div>Loading movie information...</div>;
-  // }
+  if (!movie) {
+    return <div>Loading movie information...</div>;
+  }
 
-  // const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <MovieCard />
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,24 +1,7 @@
 import React from 'react';
 
-const { title, director, metascore, stars } = movie;
 const MovieCard = props => {
-  return(
-      <div className="movie-card">
-      <h2>{props.title}</h2>
-      <div className="movie-director">
-        Director: <em>{props.director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{props.metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {props.stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-)};
+  return;
+};
 
 export default MovieCard;
