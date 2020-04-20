ed926bed3aef957231da259dc7864cc3b58a54ad
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-20 14:04:01 -0700

    completed project and stretch

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 
 const MovieCard = props => {
-  return;
+
+  const { movie } = props;
+  const { title, director, metascore, stars, id } = movie;
+  
+  const history = useHistory();
+  
+
+  return(
+
+<div className="save-wrapper">
+      <div onClick={() => history.push(`/movies/${id}`)} className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+
+
+  )
 };
 
 export default MovieCard;
