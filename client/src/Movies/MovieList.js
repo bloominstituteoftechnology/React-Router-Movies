bc7e455fba95a528c542193475c3fdc03f4d7eaf
Aidan Dang <aidandang@gmail.com>
2020-04-22 20:10:57 -0700

    fininshed the project, MVP parts

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
+  
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+        <Link key={movie.id} to={`/movies/${movie.id}`}><MovieDetails  movie={movie}/></Link> 
       ))}
     </div>
   );
