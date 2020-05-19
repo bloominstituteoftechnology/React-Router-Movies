1933a14233623165b5fedabbfb47a67f34317bd8
manuel vega <vega1340@gmail.com>
2020-05-19 08:40:01 -0700

    finished mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ import SavedList from './Movies/SavedList';
+ import MovieList from './Movies/MovieList';
+ import Movie from './Movies/Movie';
+ import { Route, Switch, Link } from 'react-router-dom'
+ 
 
-import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
 
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovieList(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    getMovies();
+  }, []);
+
+ 
+  
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
@@ -12,7 +34,17 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+     <Switch>
+    <Route exact path={`/`}>
+      <MovieList movieList={movieList}/>
+    </Route>
+      <Route path={`/movies/:id`}>
+        <Movie params={movieList}/>
+
+        
+      </Route>
+      
+      </Switch>
     </div>
   );
 };
