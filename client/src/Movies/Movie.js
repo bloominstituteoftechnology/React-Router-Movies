049290d09459b91e9c0bcc1b3ac0f49c739741ec
Jamar Torres <Jetorres88@gmail.com>
2020-05-25 18:59:39 -0400

    completed with React Hooks and stretch goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,57 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
 import MovieCard from './MovieCard';
 
-export default class Movie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
+const Movie = (props) => {
+  const [movie, setMovie] = useState();
+  const {id} = useParams()
+  const history  = useHistory()
+  useEffect(() => {
+    // const id = 1;
+    // change ^^^ that line and grab the id from the URL
+    // You will NEED to add a dependency array to this effect hook
 
-  componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = this.props.match.params.id;
-    this.fetchMovie(id);
-  }
+       axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(response => {
+          setMovie(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
 
-  fetchMovie = id => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
-        this.setState(() => ({ movie: response.data }));
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-  // Uncomment this code when you're ready for the stretch problems
-  componentWillReceiveProps(newProps){
-    if(this.props.match.params.id !== newProps.match.params.id){
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
-
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie)
-  }
-
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
-    
-    return (
-      <div className="save-wrapper">
-        <MovieCard 
-        movie={this.state.movie} 
-        />
-      <div className="save-button" onClick={() => this.saveMovie()}>Save</div>
+  },[id]);
   
-      </div>
- 
-    );
+  //Uncomment this only when you have moved on to the stretch goals
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
   }
+
+  if (!movie) {
+    return <div>Loading movie information...</div>;
+  }
+
+  return (
+    <div>
+      <MovieCard  movie={movie}/>
+      <div className="save-button" onClick={() => saveMovie(movie)}>Save Movie</div>
+      <div className="save-button-back" onClick={() => history.goBack()}>Go Back</div>
+    </div>
+  );
 }
+
+export default Movie;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const SavedList = props => {
+  return (
+  <div className="saved-list">
+    <h3>Saved Movies:</h3>
+    {props.list.map(movie => (
+      <NavLink activeClassName='activeNavButton' to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>
+    ))}
+   <NavLink activeClassName='activeNavButton' to={'/'}><div className="home-button">Home</div>
+   </NavLink> 
+  </div>
+  )
+};
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <NavLink to={`/movies/${movie.id}`}>
-            <span className="saved-movie">{movie.title}</span>
-           </NavLink>
-        ))}
-      
-        <NavLink activeClassName='activeNavButton' to='/'>
-          <div className="home-button">Home</div>
-        </NavLink>
-      </div>
-    );
-  }
-}
+export default SavedList;
