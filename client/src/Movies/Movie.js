d8f0eb3d0054ff20cc7d29523b5786087ee870dd
manuel vega <vega1340@gmail.com>
2020-06-05 14:59:11 -0700

    finished mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+ const {id} = useParams()
   useEffect(() => {
-    const id = 1;
+    
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -21,15 +23,15 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  //const saveMovie = () => {
+    //const addToSavedList = props.addToSavedList;
+    //addToSavedList(movie)
+  //}
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
+   
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
@@ -49,7 +51,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <button className="save-button">Save</button>
     </div>
   );
 }
