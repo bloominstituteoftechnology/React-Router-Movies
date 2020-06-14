951b8ca30dff7a4430d0ef88ec43ed1f8cfdb201
amber-robinson-2 <balleware@gmail.com>
2020-06-13 22:45:46 -0500

    Mission Complete! MVP met + Streach

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import axios from "axios";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
 
-  const addToSavedList = movie => {
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get("http://localhost:5000/api/movies")
+        .then((response) => {
+          setMovieList(response.data);
+        })
+        .catch((error) => {
+          console.error("Server Error", error);
+        });
+    };
+    getMovies();
+  }, []);
+
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+    <BrowserRouter>
+      <div>
+        <SavedList list={savedList} />
+        <Route exact path="/">
+          <MovieList movies={movieList} />
+        </Route>
+        <Route path="/movies/:id">
+          <Movie addToSavedList={addToSavedList} />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 };
 
