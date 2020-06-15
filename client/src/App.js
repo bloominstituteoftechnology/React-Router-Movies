8226da56a8b324790d512340029caecc2d594825
bekah <elisaalvarez211@gmail.com>
2020-06-15 16:36:25 -0700

    finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
+import { Router, Route, Link, Switch } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,23 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+      
+      
+      <Switch>
+        <Route exact path="/">
+
+         <MovieList movies={movieList} />
+
+        </Route>
+         
+        <Route path={`/movies/:movieid`}>
+
+          <Movie movie={movieList} />
+
+        </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   return (
@@ -13,7 +14,9 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to = {`/movies/${movie.id}`}>
     <div className="movie-card">
+       
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -28,7 +31,9 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      
     </div>
+    </Link>
   );
 }
 
