09d2368b0a4397b46f3be5013ff8e1f686581b10
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-06-16 15:11:48 -0500

    completed all mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useRouteMatch } from 'react-router-dom';
 
 const Movie = (props) => {
+
   const [movie, setMovie] = useState();
+  const { url } = useRouteMatch();
  
   useEffect(() => {
-    const id = 1;
+    const id = url;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api${id}`)
         .then(response => {
           setMovie(response.data);
+          window.scrollTo(0, 0);
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[]);
+  }, [url]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
