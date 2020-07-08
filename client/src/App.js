9e1fbb34d029673d2bdb965629b43601801f0f49
Donavan Terranova <dterran2@outlook.com>
2020-07-07 19:49:02 -0600

    started

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route path='/' component={MovieList}/>
+        <Route path='/movies/:id' component={Movie}/>
+      </div>
     </div>
   );
 };
