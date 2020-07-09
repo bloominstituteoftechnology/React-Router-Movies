82e92c45992f550bc229c12ff44e8f5a691a9302
Angela Plank <angelaplanknm@gmail.com>
2020-07-09 12:51:47 -0600

    standardized photo size and moved title around

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -4,8 +4,8 @@ const MovieCard = (props) => {
   const {title, director, metascore, stars, imgURL} = props.movie;
   return (
     <div className="movie-card">
-      <img src={imgURL} alt="movie-poster"/>
       <h2>{title}</h2>
+      <img src={imgURL} alt="movie-poster" height={400} width={300}/>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
