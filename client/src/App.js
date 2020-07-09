d5607e6d2fa156933a3ba5e877358a3f71eebc3a
ShirleyLe3 <synchrotronflux@yahoo.com>
2020-07-08 22:00:51 -0400

    progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import './App.css';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -11,8 +16,33 @@ const App = () => {
 
   return (
     <div>
+      {/* <Route path='/' render={props=>} */}
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div className="App">
+        <header className="App-header">
+          <BrowserRouter>
+              <Link to="/">Home</Link>
+              <Link to={{
+                    pathname: "/about", 
+                    search: "?nam=about", 
+                    hash: "#theAbout",                               state: {
+                      from: "home page", 
+                },     
+              }}>About</Link>
+
+              <Link to="/nav3">Nav3</Link>
+
+              <Route exact path="/" component={Home} 
+              Route path='/users/:id' render={props => {
+                const {id} = props.match.params;
+              return <div>The id is {id}</div>}}/>
+
+              <Route exact path="/about" component={About} render={props => { return <h3>About stuff nobody cares</h3>;}}/>
+              <Route exact path="/nav3" component={Nav3} />
+          </BrowserRouter>
+
+        </header>
+      </div>
     </div>
   );
 };
