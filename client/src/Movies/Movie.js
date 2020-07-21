35d47ac946dcee3abe49c692f551cdd854a4d762
unknown <olem2233@gmail.com>
2020-07-20 23:33:21 -0700

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -7,21 +7,19 @@ const Movie = (props) => {
   const param = useParams()
   useEffect(() => {
    
-    
-
   const id = param.id
-})
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${param.id}`)
+        .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
         })
         .catch(error => {
-          console.error(error);
-        },[]);
+          console.error(error)});
+  },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => {
