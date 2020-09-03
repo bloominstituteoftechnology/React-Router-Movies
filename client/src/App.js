0045887f167d2448243895203186662975d5749f
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-02 21:42:31 -0400

    Styled and Almost At MVP

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Movie from './Movies/Movie';
 import MovieList from './Movies/MovieList';
+import { Card } from "reactstrap"
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
@@ -27,16 +28,18 @@ const App = () => {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+    setSaved([...saved, id]);
   };
 
   return (
-    <div>
+    <Card style={{backgroundImage: "url(https://www.tvovermind.com/wp-content/uploads/2018/06/32-2.jpg)", backgroundPosition: "center" }}>
       <SavedList list={[SavedList]} />
       <div>
       <Route exact path ="/" ><MovieList movies={movieList} /></Route>
        <Route path = "/movies/:id" component={Movie} />
       </div>
-    </div>
+      
+    </Card>
   );
 };
 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
+import { Card, CardTitle, Button } from "reactstrap"
+import { Link } from "react-router-dom";
+
+const clickToHome = event => {
+  <Link to={`/`}>
+  </Link>
+}
 
 
 const SavedList = props => (
-  
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
+  <Card style={{margin: "5%", padding: "1.5%", display: "flex", flexFlow: "row", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", backgroundColor: "#edf6f9", border: "solid 1px black"}}>
+    <CardTitle style={{fontSize: "1.7rem", fontFamily: "'Krona One', sans-serif", color: "#e63946", textShadow: "1px 1px 3px #9a8c98", fontWeight: "bold", marginLeft: "3%" }}>Saved Movies:</CardTitle>
+
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button" >Home</div>
-  </div>
+
+    <Button style={{width: "80px", height: "45px", marginLeft: "65%", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", backgroundColor: "#e63946", fontFamily: "'Krona One', sans-serif", fontSize: "0.8rem" }} onClick={clickToHome} >Home</Button>
+
+  </Card>
 );
 
 
