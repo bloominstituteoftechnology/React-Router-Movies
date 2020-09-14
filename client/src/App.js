a5f193b7243b0a546db1494975d7bdf3a95feb6f
Miah <revolution5117@gmail.com>
2020-09-14 15:56:31 -0700

    Project complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,7 +14,8 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
-          // Study this response with a breakpoint or log statements
+          setMovieList(response.data)
+           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
         .catch(error => {
@@ -29,8 +32,10 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = '/'>
+        <MovieList movies = {movieList}/>
+      </Route>
+      <Route  path = '/movie/:id'><Movie movie = {movieList}/></Route>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Link, useHistory} from 'react-router-dom'
 
 export default function MovieList(props) {
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -12,9 +14,10 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  const history = useHistory()
   return (
-    <div className="movie-card">
+    
+    <div onClick = {() => history.push(`/movie/${props.movie.id}`)} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -23,5 +26,6 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+    
   );
 }
