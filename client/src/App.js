a6d07f75c4a3afb9107735562adbab5288aff6df
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-30 16:12:46 -0700

    Start off

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data);
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +32,10 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>Replace this Div with your Routes
+
+
+      </div>
     </div>
   );
 }
