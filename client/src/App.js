7afeecdca5a8fd433038cd188f1f3b8a1ef6208b
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-30 17:06:01 -0700

    Only returns the id as object object

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,7 @@ export default function App () {
     <SavedList list={[ /* This is stretch */]} />
     <div>
       <Switch>
+        
         <Route exact path="/">
           <MovieList movies={movieList} />
         </Route>
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function MovieList(props) {
   return (
     <div className="movie-list">
@@ -10,10 +10,11 @@ export default function MovieList(props) {
   );
 }
 
-function MovieDetails(props) {
+function MovieDetails(props,key) {
   const { title, director, metascore } = props.movie;
 
   return (
+    <Link to={`/movies/${key}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -23,5 +24,6 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+  </Link>
   );
 }
