6640fffdb9cb733144d5aedae06397d9ae72e226
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2020-10-28 20:01:55 -0600

    Inde.js and App.js done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const id = params.id;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
+        console.log(response);
+        console.log();
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
       })
