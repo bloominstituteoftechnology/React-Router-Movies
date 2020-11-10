b6572063ecd7de7069c147a507d85e2635ffb3a8
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-11-09 17:35:12 -0800

    working on functionality... finding the right path. Can change URL but not content

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import { Route, Switch } from "react-router-dom";
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,9 +14,11 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(res => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(res.data)
+         
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +34,17 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+    
+    <Switch>
+      <Route path= '/movie-list/:movieID'>
+      <Movie/>
+      
+      </Route>
+      <Route path = "/">
+        <MovieList movies= {movieList}></MovieList>
+      </Route>
+      
+    </Switch>
     </div>
   );
 }
