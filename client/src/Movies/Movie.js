41ba5b7b147e89d0ab7ac893b23ea5a25d99eae9
Trey <treywroberts@gmail.com>
2020-11-09 19:04:50 -0500

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, NavLink, Route, useRouteMatch, useHistory } from 'react-router-dom'
 import axios from 'axios';
+import SavedList from './SavedList';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
-  let id = 1;
+  const { movies } = props
+  const { id } = useParams()
+  const { url, path } = useRouteMatch()
+  console.log(id);
+  
+  
+  // const item = items.find(item => {
+  //   return item.id == id
+  // }) || {}
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -13,6 +22,7 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data)
       })
       .catch(error => {
         console.error(error);
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import {useHistory} from 'react-router-dom'
+ 
 export default function SavedList(props) {
+  const history = useHistory()
+  const routeBackHome = ()=>{
+    history.push('/')
+
+  }
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+       <button onClick={routeBackHome} className="home-button">
+        Home
+      </button>
     </div>
   );
 }
