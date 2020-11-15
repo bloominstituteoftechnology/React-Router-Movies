ec04d2eafc2255b41d137e2ea74c7d9c7e0d97e4
Caroline <carolinefallonlucas@gmail.com>
2020-11-14 23:03:51 -0500

    react-router-movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
 export default function MovieList(props) {
+  const { url, path } = useRouteMatch();
   return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+    <div className='movie-list'>
+      {props.movies.map((movie) => (
+        <Link to={`/movies/${movie.id}`} key={movie.id}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
 
   return (
-    <div className="movie-card">
+    <div className='movie-card'>
       <h2>{title}</h2>
-      <div className="movie-director">
+      <div className='movie-director'>
         Director: <em>{director}</em>
       </div>
-      <div className="movie-metascore">
+      <div className='movie-metascore'>
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
