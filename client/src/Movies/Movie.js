26bad1291880db04ebab7a121cca5c53c7fa18f2
Louis Stevens <loustevenshere@gmail.com>
2020-12-14 18:04:15 -0500

    Finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
+  const history = useHistory();
   const { id }  = useParams();
   console.log('Id for movies:' + id);
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
+  
 
   useEffect(() => {
     axios
