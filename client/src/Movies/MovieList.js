8619512cd3758ac83c8f1a5a1c5f7eaa6d362436
Jean Luciano <jeanluciano@mailbox.org>
2020-12-14 17:01:14 -0600

    list done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function MovieList(props) {
   return (
@@ -14,7 +15,8 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${props.movie.id}`}>
+      <div className="movie-card" >
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -23,5 +25,7 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+    </Link>
+    
   );
 }
