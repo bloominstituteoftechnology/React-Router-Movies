2db8b19d72f9c6d56b44c8f87702d31e4aa86859
Dustin Brinkman <mimirs.spring@gmail.com>
2020-12-14 19:15:27 -0500

    Was a lot of fun

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+// import {useHistory} from 'react-router-dom'
+
 
 export default function MovieList(props) {
+  const movies = props;
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -11,10 +15,11 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick= {(evt) => 
+    history.push(`/movies/${id}`)}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
