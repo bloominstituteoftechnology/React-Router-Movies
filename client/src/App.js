a1ea373cde91b00cc3d3c97d48d9a6beb669ac5f
Brian Reisman <brianreisman@protonmail.com>
2020-12-14 19:25:49 -0500

    second commit, components wired up still making URLs dynamic

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
+console.log(movieList);
 
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+        .then(res => {
+          setMovieList(res.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -22,14 +24,22 @@ export default function App () {
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+  // const addToSavedList = id => {
+  //   // This is stretch. Prevent the same movie from being "saved" more than once
+  // };
 
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
+      
+      <Route exact path={`/`}>
+        <MovieList movies={movieList}/>
+      </Route>
+      
+      <Route path={`/movies/:id`}>
+        <Movie/>
+      </Route>
+      
       <div>Replace this Div with your Routes</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default function MovieList(props) {
+  console.log(useHistory);
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -12,9 +14,10 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  const {push} = useHistory();
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={()=>{push(`/movies/${props.id}`)}}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
