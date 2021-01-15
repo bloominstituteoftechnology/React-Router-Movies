f9d36d6acb0047b64b993a4484e22f5c7b19f40d
Molly Brown <mollybrown0516@gmail.com>
2021-01-14 20:01:22 -0500

    Working through the App.js, completed the first step of wrapping the App in router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { Link, Route, NavLink } from 'react-router-dom';
 import axios from 'axios';
+import MovieList from './Movies/MovieList';
 
 import SavedList from './Movies/SavedList';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  //const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies') 
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          console.log('data: ', response.data);
+          setMovieList(response.data);
+          
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -27,10 +30,14 @@ export default function App () {
   };
 
   return (
+  
     <div>
+      <Route path='/'><MovieList/></Route>
+
       <SavedList list={[ /* This is stretch */]} />
 
       <div>Replace this Div with your Routes</div>
     </div>
+  
   );
 }
