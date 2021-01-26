1e02f894eefd2384aba70ae45f94453ba9ec4cf5
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-01-25 20:50:56 -0500

    message

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function MovieCard (props) {
-  return;
+export default function MovieCard(props) {
+	return;
+	// console.log("from movie card" + props.movie);
+	// return (
+	// <div>
+	// <h1>im ready</h1>
+	// </div>
+	// );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function MovieList(props) {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+	const history = useHistory();
+	return (
+		<div className="movie-list">
+			{props.movies.map((movie) => (
+				<div onClick={() => history.push(`/movie-list/${movie.id}`)}>
+					<MovieDetails key={movie.id} movie={movie} />
+				</div>
+			))}
+		</div>
+	);
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+	const { title, director, metascore } = props.movie;
 
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
+	return (
+		<div className="movie-card">
+			<h2>{title}</h2>
+			<div className="movie-director">
+				Director: <em>{director}</em>
+			</div>
+			<div className="movie-metascore">
+				Metascore: <strong>{metascore}</strong>
+			</div>
+		</div>
+	);
 }
