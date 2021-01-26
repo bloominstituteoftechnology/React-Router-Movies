0c5ab74c4525145414f62bf22f3435276d5f5ce9
Jonathan Garcia <70542639+CoderJonathan@users.noreply.github.com>
2021-01-25 23:47:55 -0800

    Finally MVP done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { useRouteMatch, useHistory } from 'react-router-dom'
 
 export default function MovieList(props) {
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+          <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
+/* [ ] When a user clicks on the movie card inside `MovieList` they should be taken to `/movies/{id of clicked movie here}` to see the details of the selected movie.
+ */
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
+
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={() => history.push(`/movies/${id}`)}>
+      
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
