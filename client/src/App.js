753401b53127a0369651d3427cb354be9d577737
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-22 14:31:59 -0800

    server not working ?

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 import SavedList from './Movies/SavedList';
+import { Link, Route, Switch } from 'react-router-dom';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,6 +14,7 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          console.log(response)
           setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
@@ -31,8 +34,17 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      
+        <Route exact path='/' >
+          <MovieList title={movieList.title} director={movieList.director} metascore={movieList.metascore}  />
+        </Route>
 
-      <div>Replace this Div with your Routes</div>
+        <Route path='./movies/:id'>
+          <Movie />
+        </Route>
+
+        
+      
     </div>
   );
 }
