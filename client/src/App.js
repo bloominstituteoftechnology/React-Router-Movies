906fef3abed132b3c14bac73358e84c3c3126bb9
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-22 12:00:26 -0800

    first commit, just finished setting up a few things

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -22,6 +23,7 @@ export default function App () {
     getMovies();
   }, []);
 
+  console.log(movieList)
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
