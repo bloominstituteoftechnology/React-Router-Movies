73ae88f79231442fb0321e4d6077a1c0977b118f
jayesquivel <josephesquivel111@gmail.com>
2021-03-10 17:24:39 -0800

    finished cash money

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom'
 
 export default function MovieList(props) {
+  const {movies} = props
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -12,9 +14,13 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  const history = useHistory()
+  const routeToDets = () => {
+    history.push(`/movies/${props.movie.id}`)
+  }
 
   return (
-    <div className="movie-card">
+      <div onClick = {routeToDets} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
