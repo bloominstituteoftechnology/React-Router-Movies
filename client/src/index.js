97d5fddb3f69707f024fb199654a30e5c7d39fb5
Timothy Langham <graniteblood@gmail.com>
2021-03-22 20:54:33 -0400

    finished

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+//BrowserRouter^^^ adheres to the 'history API' - that's what we want 
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
