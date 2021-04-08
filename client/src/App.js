aa3cfb91117dbaa5f7a18336dd0e37b86d4e35ba
vida <ofo28@aol.com>
2021-04-08 02:08:46 -0400

    index js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link, Route, Switch } from 'react-router-dom'
+import "./index.css";
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +17,9 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.movieList)
+          console.log(response.movieList, 'movieList')
+
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +35,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+     
+      <div>
+      <Switch> 
+      <Route exact path="/" component={Movie}/>
+      <Route path="/MovieList" component={MovieList}/>
+      </Switch>
+      </div>
+       
     </div>
   );
 }
