80ae418013b344fb371491f28fc4bcf489e6b311
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-19 16:06:53 -0600

    update to try and get things to render

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import Movie from './Movies/Movie';
 import MovieCard from './Movies/MovieCard';
-
+import MovieList from './Movies/MovieList';
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
@@ -15,7 +15,7 @@ export default function App () {
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
           // Study this response with a breakpoint or log statements
-          console.log(response.data);
+          // console.log(response.data);
           // and set the response data as the 'movieList' slice of state
           setMovieList(response.data);
         })
@@ -33,15 +33,15 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+    <BrowserRouter>
+     <Route path='/movies'>
+       <Movie movieList ={movieList}/>
+     </Route>
 
-      <Switch>
-        <Route path='/'>
-          <MovieList list= {movieList} />
-        </Route>
-        <Route path='/movies/:id'>
-          <Movie list ={moveList} />
-        </Route>
-      </Switch>
+     <Route path='/'>
+       <MovieList movieList ={movieList}/>
+     </Route>
+    </BrowserRouter>
     </div>
   );
 }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useParams} from 'react-router-dom';
-// import MovieList from './MovieList';
-
+import { useParams } from 'react-router-dom';
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let {id} = useParams();
+  let id = 1;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -14,9 +12,8 @@ export default function Movie(props) {
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
         // Study this response with a breakpoint or log statements
-        console.log(response[0]);
+        console.log(response);
         // and set the response data as the 'movie' slice of state
-        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
