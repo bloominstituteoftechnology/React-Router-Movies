2cb9e182ead9de0b633b8d635c98951ea9407f33
cynthiaannberg <cynthia.ann.berg@me.com>
2021-04-19 21:46:39 -0600

    movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactDOM from "react-dom";
+import { Route, Link, Switch } from "react-router-dom";
+import MovieList from './Movies/MovieList'
+
+
 
 import SavedList from './Movies/SavedList';
+
+{/* <Route exact path="/" movies={SavedList}/>
+<Route path="/movie" movies={Movie}/> */}
+
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+function Home(props){
+  const { push } = props.history;
+  return <h1>Home Component</h1>;
+  <button onClick={() => push('/')}>back home</button>
+}
+function Movie(props){
+  return <h1>Movie Component</h1>;
+}
+function MovieList(props){
+  return <h1>Movie List Component</h1>;
+}
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -30,7 +57,28 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <BrowserRouter>
+    <Switch>
+      <Route path = '/movies/movie:ID'>
+        <Movie movie = {movieList}/>
+      </Route>
+      <Route path = '/'>
+        <MovieList movies = {movieList} />
+      </Route>
+    </Switch>
+    </BrowserRouter>
+      </div>
     </div>
   );
 }
+
+// <BrowserRouter>
+// <Link to='/'> Home </Link>
+// <Link to='/movie'> Movie </Link>
+// <Link to='/movielist'> Movie List </Link>
+
+// <Route  exact path='/' component={Home} />
+// <Route path='/movie' component={Movie} />
+// <Route path='/movielist' component={Movie List} />
+// </BrowserRouter>
