7c2f8cdf3bd1636cbf76ea416c436085fbb971f7
Daniel Walker <dw765608@gmail.com>
2021-04-21 20:48:35 -0400

    Finish

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function MovieCard (props) {
-  return;
-}
+const MovieCard = props => {
+  return (<div className="movie-card">
+  <h2>{props.title}</h2>
+  <div className="movie-director">
+    Director: <em>{props.director}</em>
+  </div>
+  <div className="movie-metascore">
+    Metascore: <strong>{props.metascore}</strong>
+  </div>
+  <h3>Actors</h3>
+
+
+</div>);
+};
+
+export default MovieCard;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-export default function MovieList(props) {
+
+const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getMovies();
+  }, []);
+  
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+function MovieDetails({ movie }) {
+  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    <Link to={`/movies/${movie.id}`}>
+      <MovieCard 
+        title={title}
+        director={director}
+        metascore={metascore}
+        stars={stars}
+        />
+    </Link>
   );
 }
+
+export default MovieList;
