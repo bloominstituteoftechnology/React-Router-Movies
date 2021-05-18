59602325bcde0c81406fd7a8bf2441d973cf9aab
ferengitooth <ehitsamill@gmail.com>
2021-05-17 18:12:35 -0700

    finished movie.js instructions

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ export default function App () {
           <Movie />
         </Route>
         
-        <Route path='/'>
+        <Route exact path='/'>
           <MovieList movieList={movieList}/>
         </Route>
 
