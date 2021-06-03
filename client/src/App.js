cc7b8f76df2737a2b2464a2f08c1ff2878030269
Amlak befekadu <amlakbarkon@gmail.com>
2021-06-03 00:10:15 -0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import { BrowserRouter, BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
-
+function Home(props){
+  return(
+    <div>Home</div>
+  )
+}
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
-
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -14,6 +20,9 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data)
+          setMovieList(response.data)
+          setSaved(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -24,12 +33,19 @@ export default function App () {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+   
   };
-
+ 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-
+       <SavedList list={[movieList]} />
+        <Link to ="/">Home</Link>
+        <Link to ='/movies/:id'>About</Link>
+        <Route  exact path="/" render={() => <MovieList movies={movieList}/>} />
+        <Route path="/about" component ={<Movie props={saved.id}/>} />
+    
+     
+      
       <div>Replace this Div with your Routes</div>
     </div>
   );
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,8 +4,8 @@ export default function SavedList(props) {
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie,index) => (
+        <span className="saved-movie"><p>{index,movie.title}</p></span>
       ))}
       <div className="home-button">Home</div>
     </div>
