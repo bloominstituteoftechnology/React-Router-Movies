69cc5187e1f355ac1e8f2161f72bf0162d8eb6a6
vida <ofo28@aol.com>
2021-06-02 22:47:53 -0400

    Working on the links

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Link, Switch } from "react-router-dom";
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+                setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,9 +31,20 @@ export default function App () {
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      
+      <Switch>
+      
+      <SavedList list={movieList} />
+      <Route path="/movies">
+      <Movie movies= {movieList}/>
+      </Route>
+      <Route path = "/movies/:id">
+        <MovieList movie={movieList}/>
+      </Route>
+      
 
-      <div>Replace this Div with your Routes</div>
+      </Switch>
+      
     </div>
   );
 }
