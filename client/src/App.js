b4644fc1a54a88a84fbf4d22b777940d100dc705
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-16 11:58:05 -0500

    added routes, still not working yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList.js';
+import Movie from './Movies/Movie.js';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -10,10 +12,10 @@ export default function App () {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+        .get('http://localhost:5000/api/movies') 
+        .then(res => {
+          console.log(res);
+          setMovieList(res.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +32,17 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            <MovieList movies={movieList}/>
+          </Route>
+      
+          <Route path={'/movies/:id'}>
+            <Movie />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
