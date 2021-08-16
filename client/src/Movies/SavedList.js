c9daa44bb0b392c2a23aabb8ef2ebf3c95553e05
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-16 15:49:57 -0600

    Have the basic tasks working

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { NavLink, useRouteMatch } from 'react-router-dom';
 
 export default function SavedList(props) {
+
+  const { path, url} = useRouteMatch();
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <NavLink to="/">
+        <div className="home-button">Home</div>
+      </NavLink>
     </div>
   );
 }
