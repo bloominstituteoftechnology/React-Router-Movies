986c04d7da53490e6b0a3e733ebaaee32b6d369d
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-08-16 18:55:11 -0700

    App, MovieList, Movie

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
-export default function MovieList(props) {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
+export default function MovieList(props)
+{
+    const { movies } = props;
+
+    // We'll grab the current URL using the hook
+    const { url } = useRouteMatch();
+
+    // console.log("URL:", url);
+
+    return (
+        <div className="movie-list">
+            {movies.map(movie => (
+                <MovieDetails key={movie.id} movie={movie} url={url} />
+            ))}
+        </div>
+    );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+function MovieDetails(props)
+{
+    const { title, director, metascore, id } = props.movie;
+    const { url } = props;
 
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
+    // console.log("ID:", id);
+
+    return (
+        <div className="movie-card">
+            <Link to={`${url}/${id}`}>
+                <h2>{title}</h2>
+            </Link>
+            <div className="movie-director">
+                Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+                Metascore: <strong>{metascore}</strong>
+            </div>
+
+        </div>
+    );
 }
