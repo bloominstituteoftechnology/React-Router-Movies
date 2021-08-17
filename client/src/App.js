1bceb302969a7897e7d2c8646376750ce14156ac
KRQuinn <kr.quinn82@gmail.com>
2021-08-16 22:43:47 -0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,12 +14,14 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(res => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(res.data);
+          
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch(err => {
+          console.error('Server Error', err);
         });
     }
     getMovies();
@@ -30,7 +35,17 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path={`/movies/:movieId`}>
+          <Movie movies={movieList}/>
+        </Route>
+        <Route exact path='/'>
+          <MovieList movies={movieList}/>
+        </Route>
+        {/* <Route path={`/movies/:movieId`} component={Movie}/> */}
+
+
+      </Switch>
     </div>
   );
 }
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
-  let id = 1;
+  const { movies } = props;
+  const { movieId } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
+  const id = movies.find(item=>item.id === movieId);
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
-      .then(response => {
+      .get(`http://localhost:5000/api/movies/${movieId}`) // Study this endpoint with Postman
+      .then(res => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(res.data)
+        console.log(res.data)
       })
-      .catch(error => {
-        console.error(error);
+      .catch(err => {
+        console.error(err);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
