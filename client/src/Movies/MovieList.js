47b9d7ff123eca7ef0ddd7ba30d89623b733c0a2
Rick <ricksmycodeguy@gmail.com>
2021-09-24 19:43:22 -0500

    saftey push

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,18 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Link, useHistory } from 'react-router-dom';
-// import { useParams } from 'react-router-dom'
-
-export default function MovieList(props) {
-  console.log('MovieList.js ln:8 props', props);
-=======
 import { Link } from 'react-router-dom';
 // import { useParams } from 'react-router-dom'
 
 
 
 export default function MovieList(props) {
->>>>>>> f432b5f74740ff0ada66833114c5193ff929dab2
  
   return (
     <div className="movie-list">
@@ -20,11 +12,8 @@ export default function MovieList(props) {
         <Link key={movie.id} to={`/Movies/MovieList/${movie.id}`}> 
         <MovieDetails key={movie.id} movie={movie} />
         </Link>
-<<<<<<< HEAD
-=======
        
         
->>>>>>> f432b5f74740ff0ada66833114c5193ff929dab2
       ))}
     </div>
   );
