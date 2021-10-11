e76dbda1d5b6589057fe32d9fb8d738c948f9f7a
Ryan Howard <ryanghoward@protonmail.com>
2021-10-11 15:00:10 -0700

    Two routes set up and functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,54 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { Route, Switch } from 'react-router-dom';
+// import Movie from "./Movies/Movie"
+
+// import SavedList from './Movies/SavedList';
+// import MovieList from './Movies/MovieList';
+
+// export default function App () {
+//   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+//   const [movieList, setMovieList] = useState([]);
+
+// useEffect(() => {
+//   const getMovies = () => {
+//     axios.get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+//       .then(response => {
+//         setMovieList(response.data)
+//         })
+//         .catch(error => {
+//           console.error('Server Error', error);
+//         });
+//     }
+//     getMovies();
+//   }, []);
+
+//   const addToSavedList = id => {
+//     // This is stretch. Prevent the same movie from being "saved" more than once
+//   };
+
+//   return (
+//     <div>
+//       <SavedList list={[ /* This is stretch */]} />
+
+//       <Switch>
+//         <Route path="/movies/:id">
+//           <Movie />
+//           </Route>
+//         <Route path="/">
+//           <MovieList movies={movieList}/>
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
+import Movie from './Movies/Movie'
+import MovieList from './Movies/MovieList'; 
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -9,9 +57,9 @@ export default function App () {
 
   useEffect(() => {
     const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+      axios.get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -29,8 +77,14 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path='/movies/:id'>
+          <Movie />
+        </Route>
+        <Route path='/'>
+          <MovieList movies={movieList}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
