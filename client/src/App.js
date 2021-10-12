653927c0ace1f6df8e23a124394099ce2b77130d
brad <bradley.michaels@outlook.com>
2021-10-11 23:26:19 -0400

    figuring it out

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import Movie  from './Movies/Movie';
+import MovieList  from './Movies/MovieList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
-
   useEffect(() => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(res => {
+            console.log(res.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(res.data);
+
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -21,16 +25,25 @@ export default function App () {
     }
     getMovies();
   }, []);
-
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
+ 
 
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Movie />
+      <div>
+      <BrowserRouter>
+        <Link to="/">MovieList</Link>
+        </BrowserRouter>
+        
 
-      <div>Replace this Div with your Routes</div>
+        
+        
+       
+      </div>
     </div>
   );
 }
