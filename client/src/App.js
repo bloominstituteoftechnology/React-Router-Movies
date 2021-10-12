3a46258c055d739dbc01b8b5c8d107ca15aa4582
David - Lambda School User <david-lambdaschooluser@davids-mbp.lan>
2021-10-12 10:39:57 -0400

    MVP functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom'
 
+import MovieList from './Movies/MovieList'
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie'
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies')
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -22,15 +23,15 @@ export default function App () {
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
-
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/' render={(props) => {
+        return <MovieList movies={movieList}></MovieList>
+      }}></Route>
+     <Route path='/movies/:id'>
+       <Movie></Movie>
+     </Route>
     </div>
   );
 }
