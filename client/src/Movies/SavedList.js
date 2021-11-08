210aa088622774d90243a96b1e5e8aec2e6d0cbf
Bobby Gomez <bobbygomez@Bobbys-MBP.local.tld>
2021-11-08 16:12:11 -0600

    changed home link code

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function SavedList(props) {
-  const history = useHistory();
-    const routeToHome = () => {
-        history.push("/");
-    }
+ 
+
+    
     return (
         <div className="saved-list">
             <h3>Saved Movies:</h3>
             {props.list.map(movie =>(
                 <span className="saved=movie">{movie.title}</span>
             ))}
-            <div onClick={routeToHome} className="home-button">Home</div>
+             <Link to = '/' className = 'home-button'>Home</Link>
         </div>
     );
 } 
