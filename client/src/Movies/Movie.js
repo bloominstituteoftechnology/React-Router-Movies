ea0c086773ead3075efc4bda4a53961e129c8a7e
selinax19 <89764911+selinax19@users.noreply.github.com>
2021-11-08 16:42:10 -0500

    finished

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  let fetchId = useRouteMatch();
+  let id = fetchId.params.id; 
+  let { id } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -13,6 +17,7 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
@@ -20,16 +25,12 @@ export default function Movie(props) {
     // This effect should run every time time
     // the `id` changes... How could we do this?
   }, []);
-
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
-
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
   const { title, director, metascore, stars } = movie;
-
   return (
     <div className="save-wrapper">
       <div className="movie-card">
@@ -41,7 +42,6 @@ export default function Movie(props) {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
