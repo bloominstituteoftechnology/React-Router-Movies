8ed30ea4c9ffe6e22d8622c1c466a2e04d8d1da4
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2021-12-13 18:19:58 -0500

    completed app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 
+import { Route, Switch } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import MovieCard from './Movies/MovieCard';
+
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
@@ -10,8 +14,9 @@ export default function App () {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -27,10 +32,16 @@ export default function App () {
   };
 
   return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
-    </div>
+    <Switch>
+      <Route path='/movies/:id'>
+        <MovieCard />
+      </Route>
+      <Route path='/movies/saved'>
+        <SavedList list={[ /* This is stretch */]} /> 
+      </Route>
+      <Route path='/'>
+        <MovieList movies={movieList} title={movieList.title} director={movieList.director} metascore={movieList.metascore} />
+      </Route>
+    </Switch>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
 // You'll need to wrap <App /> for routing to work
 ReactDOM.render(
-<BrowserRouter>
+<Router>
     <App />
-</BrowserRouter>, 
+</Router>, 
 document.getElementById('root'));
