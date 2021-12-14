41c0be07ea55cb29c3bdb932147dfe756894a5ba
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2021-12-13 17:04:10 -0800

    ready

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+      // Wrap the <App /> in a provider
+  <Router>
+  <App />
+</Router>
+, document.querySelector('#root')
+)
+{/* <Router>
+    <App />
+</Router>, document.getElementById('root'));
+) */}
