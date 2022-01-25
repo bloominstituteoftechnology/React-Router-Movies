d41f310e902a1cb22a9fa7e8c8d95e4ae786b908
eli-kirschbaum <eli.kirschbaum1@gmail.com>
2022-01-24 22:07:05 -0600

    added the two routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -27,10 +32,16 @@ export default function App () {
   };
 
   return (
+    <Router>
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component={MovieList} />
+        <Route path='./Movies/:Movie' component={Movie}/>
+          <Movie key={Movie.id} movie={Movie} />
+      </div>
     </div>
+    </Router>
   );
 }
