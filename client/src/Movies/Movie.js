73ab14721056da1c0f48e9ee8fc96c3afd0c5836
Ricardorouge <ricky032929@gmail.com>
2022-02-22 09:14:45 -0500

    did this project in the morning had to leave to work

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import{useParams} from 'react-router-dom'
 import axios from 'axios';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  const {itemID} = useParams();
 
-  let id = 1;
+  let id = itemID;
+  
+  
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
+        setMovie(response.data);
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
       })
