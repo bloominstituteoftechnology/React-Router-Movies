6ae63f7bdf2d1423a8373e04e4c50445f07dad40
Gage Clancy <gclancy121@gmail.com>
2022-03-28 17:06:19 -0500

    updates

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {useRouteMatch} from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  let id = movie;
+  console.log(id);
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
