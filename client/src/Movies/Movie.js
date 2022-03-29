8a2fe8a44df160a630c44ba9f27065a05d249cdb
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-03-28 20:40:18 -0400

    Finished Movie Component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { 
+  useParams, 
+  useRouteMatch, 
+  NavLink, 
+  Route,
+  Switch
+} from "react-router-dom";
 import axios from 'axios';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { id } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
@@ -13,6 +20,7 @@ export default function Movie(props) {
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
